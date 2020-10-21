9e6c2422304155c2ff2c199024b8007223845532
JCEC007 <jcec007@gmail.com>
2020-10-21 18:45:15 -0500

    +Fix: SettingApiController | transformer: checkbox default value

--- Http/Controllers/Api/SettingApiController.php
+++ Http/Controllers/Api/SettingApiController.php
@@ -25,13 +25,13 @@ class SettingApiController extends BaseApiController
   private $session;
   public function __construct(SettingRepository $settings, Store $session)
   {
-
+    
     $this->settings = $settings;
     $this->module = app('modules');
     $this->session = $session;
   }
-
-
+  
+  
   /**
    * GET ITEMS
    *
@@ -42,13 +42,13 @@ class SettingApiController extends BaseApiController
     try {
       //Get Parameters from URL.
       $params = $this->getParamsRequest($request);
-
+      
       $modulesWithSettings = $this->settings->moduleSettings($this->module->allEnabled());
-
+      
       $dbSettings = [];
       $translatableSettings = [];
       $plainSettings = [];
-
+      
       // fetching translatable, plain, and DB setting by each module enabled with settings
       foreach ($modulesWithSettings as $key => $module){
         $translatableSettings[$key] = $this->settings->translatableModuleSettings($key);
@@ -62,22 +62,22 @@ class SettingApiController extends BaseApiController
           $assignedSettings = $params->settings['assignedSettings'];
         }
       }
-
+      
       // merging translatable and plain settings
       $mergedSettings = array_merge_recursive($translatableSettings,$plainSettings);
-
+      
       $response = ["data" => $this->transformSettings($mergedSettings, $dbSettings, $assignedSettings)];
-
+      
     } catch (\Exception $e) {
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
     }
-
+    
     //Return response
     return response()->json($response, $status ?? 200);
   }
-
-
+  
+  
   /**
    * GET A ITEM
    *
@@ -89,26 +89,26 @@ class SettingApiController extends BaseApiController
     try {
       //Get Parameters from URL.
       $params = $this->getParamsRequest($request);
-
+      
       $module = $this->module->find($criteria);
-
+      
       //Break if no found item
       if(!$module) throw new \Exception('Item not found',404);
-
+      
       $this->session->put('module', $module->getLowerName());
       $dbSettings = $this->settings->findByModule($module->getLowerName());
       //Response
       $response = ["data" =>  SettingTransformer::collection($dbSettings)];
-
+      
     } catch (\Exception $e) {
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
     }
-
+    
     //Return response
     return response()->json($response, $status ?? 200);
   }
-
+  
   /**
    * UPDATE ITEM
    *
@@ -120,11 +120,11 @@ class SettingApiController extends BaseApiController
   {
     \DB::beginTransaction(); //DB Transaction
     try {
-
+      
       $data = $request->input('attributes');
-
+      
       $this->settings->createOrUpdate($data);
-
+      
       //Response
       $response = ["data" => 'Item Updated'];
       \DB::commit();//Commit to DataBase
@@ -133,29 +133,29 @@ class SettingApiController extends BaseApiController
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
     }
-
+    
     //Return response
     return response()->json($response, $status ?? 200);
   }
-
+  
   public function transformSettings(&$mergedSettings, $dbSettings, $assignedSettings){
     $transformedModules = [];
-
+    
     foreach ($mergedSettings as $keyModule => &$module){
-
+      
       foreach ($module as $keySetting => &$setting) {
-
+        
         // name of setting in DB setting
         $dbSettingName = strtolower($keyModule) . '::' . $keySetting;
-
+        
         if (empty($assignedSettings) || in_array($dbSettingName, $assignedSettings)) {
-
-
+          
+          
           if (isset($dbSettings[$keyModule][$dbSettingName])) {
-
+            
             // merging data on DB with config setting
             $settingTransformed = new SettingTransformer($dbSettings[$keyModule][$dbSettingName]);
-
+            
             $setting = array_merge(collect($settingTransformed)->toArray(), $setting);
           } else {
             // init setting value if is file
@@ -166,57 +166,61 @@ class SettingApiController extends BaseApiController
                 $setting['value'] = [];
               else
                 $setting['value'] = '';
-
+            
             // init setting name
             $setting['name'] = $dbSettingName;
-
+            
           }
-
+          
           if (isset($setting['plainValue'])) {
             // decode plain value if is object or array
             $plainValue = $setting['plainValue'];
             $plainValue = $this->isJson($plainValue) ? json_decode($plainValue) : $plainValue;
-
+            
             // update plain value
             $setting['plainValue'] = $plainValue;
-
+            
             // setting value off settings not translatable
             $setting['value'] = $plainValue;
-
+            
           }
-
+          
           if (isset($setting['description'])) {
             // translate description
             $description = $setting['description'];
             $setting['description'] = trans($description);
           }
-
+          
           if (!isset($setting['isTranslatable'])) {
             $setting['isTranslatable'] = $setting['translatable'] ?? false;
           }
-
-
+          
+          
           // type setting standard based in view param
           $setting['type'] = $setting['view'];
           if (Str::contains($setting['view'], 'select'))
             $setting['type'] = 'select';
-
+          
           if (Str::contains($setting['view'], 'select-multi'))
             $setting['type'] = 'select-multi';
-
-
+          
+          
           // type selectMultiple where view contains select-locale string
           if (Str::contains($setting['view'], 'select-locales'))
             $setting['type'] = 'select-multi';
-
+          
           // init boolean value when type is checkbox
           if ($setting['type'] == 'checkbox') {
             if($setting['value']=='1')
               $setting['value'] = true;
             else
-              $setting['value']= false;
+              if($setting['value'] == '0')
+                $setting['value']= false;
+              else
+                if(isset($setting["default"]))
+                  $setting['value'] = $setting["default"];
           }
-
+          
           // type setting standard based in view param
           if (Str::contains($setting['view'], 'file')){
             $setting['type'] = 'file';
@@ -227,20 +231,20 @@ class SettingApiController extends BaseApiController
               ];
             }
           }
-
+          
           // type setting standard based in view param
           if (Str::contains($setting['view'], 'color'))
             $setting['type'] = 'color';
-
+          
           // type setting standard based in view param
           if (Str::contains($setting['view'], 'text-multi')){
-
+            
             $setting['type'] = 'text-multi';
             if(!$setting['value']){
               $setting['value'] = [];
             }
           }
-
+          
           // type setting standard based in view param
           if (Str::contains($setting['view'], 'text-multi-with-options')){
             $setting['type'] = 'text-multi-with-options';
@@ -255,29 +259,27 @@ class SettingApiController extends BaseApiController
               $setting['value'] = [];
             }
           }
-
+          
           // type setting standard based in view param
           if (isset($setting["custom"]) && $setting["custom"]){
             $setting['type'] = $setting['view'];
-            if(isset($setting["default"]) && !$setting['value']){
-              $setting['value'] = $setting["default"];
-            }
           }
-
-
+          
+          
+          
         }else{
           unset($module[$keySetting]);
         }
       }
-
+      
       if (empty($module))
         unset($mergedSettings[$keyModule]);
     }
-
+    
     return $mergedSettings;
   }
-
-
+  
+  
   function isJson($string) {
     return ((is_string($string) &&
       (is_object(json_decode($string)) ||
