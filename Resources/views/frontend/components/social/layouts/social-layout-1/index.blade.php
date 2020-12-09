4756bad6c4a10f6227c9b86748a0832b4bd8a905
Slim Arias Sanchez <slimarias1987@gmail.com>
2020-12-09 10:36:04 -0500

    +Update: social icons customization with icon array definition

--- Resources/views/frontend/components/social/layouts/social-layout-1/index.blade.php
+++ Resources/views/frontend/components/social/layouts/social-layout-1/index.blade.php
@@ -3,7 +3,7 @@
     <a href="{{ $value }}" target="_blank">
       <span class="fa-stack fa-lg">
         <i class="fa fa-{{ $type }} fa-stack-2x text-primary"></i>
-        <i class="fa fa-{{ $name }} fa-stack-1x @if($type=='square-o') text-primary @else text-white @endif"></i>
+        <i class="fa fa-{{ $name }} fa-stack-1x @if($type=='square-o' || empty($type)) text-primary @else text-white @endif"></i>
       </span>
     </a>
   @endif
