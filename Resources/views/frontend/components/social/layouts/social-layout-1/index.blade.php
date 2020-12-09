0553a40af6053cb449f16d013ff983f5ca7a9048
Slim Arias Sanchez <slimarias1987@gmail.com>
2020-12-09 16:20:13 -0500

    +Update: no force color in icons

--- Resources/views/frontend/components/social/layouts/social-layout-1/index.blade.php
+++ Resources/views/frontend/components/social/layouts/social-layout-1/index.blade.php
@@ -3,11 +3,11 @@
     <a href="{{ $value }}" target="_blank">
       @if($type)
       <span class="fa-stack fa-lg">
-        <i class="fa fa-{{ $type }} {{ strpos($name, 'fa') ? 'fa-stack-2x' : '' }} text-primary"></i>
-        <i class="{{ $name }} {{ strpos($name, 'fa') ? 'fa-stack-1x' : '' }} @if($type=='square-o' || empty($type)) text-primary @else text-white @endif"></i>
+        <i class="fa fa-{{ $type }} {{ strpos($name, 'fa') ? 'fa-stack-2x' : '' }}"></i>
+        <i class="{{ $name }} {{ strpos($name, 'fa') ? 'fa-stack-1x' : '' }} @if($type=='square-o' || empty($type))  @else text-white @endif"></i>
       </span>
       @else
-        <i class="{{ $name }} text-primary"></i>
+        <i class="{{ $name }}"></i>
       @endif
     </a>
   @endif
