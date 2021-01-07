9f0e95d4861fd4350c2031bad488d2b88c3db788
Juan Escalante <jcec007@gmail.com>
2021-01-07 12:15:21 -0500

    +Update: Menu and Logo Blade components

--- Resources/views/frontend/components/logo.blade.php
+++ Resources/views/frontend/components/logo.blade.php
@@ -1,6 +1,6 @@
 @if(!empty($logo))
   <x-media::single-image :alt="setting('core::site-name')" :title="setting('core::site-name')" :
-                      :url="$to" :isMedia="true" :zone="$zone"
+                      :url="$to" :isMedia="true" :zone="$zone" :imgClasses="$imgClasses"
                       :mediaFiles="$logo->mediaFiles()"/>
 @endif
 
