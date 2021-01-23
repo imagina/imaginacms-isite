1b76ee855ccaae88e46e5cbb9aa8a1f7d0b208ac
Juan Escalante <jcec007@gmail.com>
2021-01-23 02:50:45 -0500

    +Add: Whatsapp layout 3 and 4 | by Julianb

--- Resources/views/frontend/components/whatsapp/layouts/whatsapp-layout-2/index.blade.php
+++ Resources/views/frontend/components/whatsapp/layouts/whatsapp-layout-2/index.blade.php
@@ -1,26 +1,23 @@
 <!--Validation to show icon or dropdown -->
 @if(count($items) <= 1)
   @foreach($items as $key => $item)
-    <a href="https://wa.me/{{ $item->callingCode }}{{ $item->number }}?text={{ $item->message }}" target="_blank">
+    <a href="https://wa.me/{{ $item->callingCode }}{{ $item->number }}?text={{ $item->message }}" class="whatsapp-layout-2" target="_blank">
       <i class="icon-whatsapp {{ $icon }}"></i>
     </a>
   @endforeach
 @else
-  <div class="dropdown d-inline-block dropdown-whatsapp">
-    <!--Icon dropdown-->
-    <a class="dropdown-toggle" type="button" id="dropdownMenuWhatsapp" data-toggle="dropdown" aria-haspopup="true"
-       aria-expanded="false">
+  <div class="btn-group {{$alignment}} whatsapp-layout-2">
+    <a id="dropdownMenuWhatsapp" type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="icon-whatsapp {{ $icon }}"></i>
     </a>
-    <!--Numbers to show-->
-    <div class="dropdown-menu {{$alignment}} dropdown-menu-whatsapp"
-         aria-labelledby="dropdownMenuWhatsapp">
+    <div class="dropdown-menu dropdown-menu-whatsapp">
+      <!-- Dropdown menu links -->
       @foreach($items as $key => $item)
         @if(!empty($item->callingCode) && !empty($item->number))
           <div class="number-whatsapp">
             <a href="https://wa.me/{{ $item->callingCode }}{{ $item->number }}?text={{ $item->message }}"
                target="_blank">
-              {{ $item->formattedNumber }}
+              <span>{{ $item->formattedNumber }}</span>
             </a>
           </div>
         @endif
