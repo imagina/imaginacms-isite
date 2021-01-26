9234a3790f67ecb22eefef16539ee3da0b6149cd
Slim Arias Sanchez <slimarias1987@gmail.com>
2021-01-26 11:40:11 -0500

    +FIX: social stacked icons style

--- Resources/views/frontend/components/social/layouts/social-layout-1/index.blade.php
+++ Resources/views/frontend/components/social/layouts/social-layout-1/index.blade.php
@@ -4,8 +4,8 @@
       <a href="{{ $value }}" target="_blank">
         @if($type)
         <span class="fa-stack fa-lg">
-          <i class="fa fa-{{ $type }} {{ strpos($name, 'fa') ? 'fa-stack-2x' : '' }}"></i>
-          <i class="{{ $name }} {{ strpos($name, 'fa') ? 'fa-stack-1x' : '' }} @if($type=='square-o' || empty($type))  @else text-white @endif"></i>
+          <i class="fa fa-{{ $type }} fa-stack-2x"></i>
+          <i class="{{ $name }} fa-stack-1x @if($type=='square-o' || empty($type))  @else text-white @endif"></i>
         </span>
         @else
           <i class="{{ $name }}"></i>
