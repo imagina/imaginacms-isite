22bbb6cbc8b3f543492f78e01832bc39cd5833d6
Juan Escalante <jcec007@gmail.com>
2021-02-09 17:37:13 -0500

    +Update: Set default Title to each layouts

--- View/Components/Menu.php
+++ View/Components/Menu.php
@@ -34,7 +34,7 @@ class Menu extends Component
     $this->repository = $repository;
     $this->params = $params;
     $this->layout = $layout;
-    $this->title = $title;
+    $this->title = $this->layout == 'category-menu-layout-2' ? ($title == "Categorías" ? "" : $title) : $title;
     $this->menuBefore = $menuBefore;
     $this->menuAfter = $menuAfter;
     $this->withHome = $withHome;
