5a587136ae0a5f8fa1e2e56d9c275c4c34f96f57
William Verde <williamverde10@gmail.com>
2021-02-16 17:15:58 -0400

    +Update: pagination fixes

--- Resources/views/frontend/livewire/index/item-list.blade.php
+++ Resources/views/frontend/livewire/index/item-list.blade.php
@@ -17,24 +17,13 @@
 	                  :item="$item"
 	                  :itemListLayout="$itemListLayout"
 	                />
-	                
+	               
 				</div>
 				@endforeach
 			</div>
 
 			@if($pagination["show"])
-				<div class="row">
-					<div class="{{$entityName}}-list-pagination d-flex w-100 px-3 justify-content-end">
-
-						@if($pagination["type"]=="normal")
-							{{ $items->links('isite::frontend.livewire.index.custom-pagination') }}
-						@endif
-						@if($pagination["type"]=="loadMore")
-							<a wire:click="loadMore" class="btn btn-primary btn-load-more">{{trans('isite::frontend.buttons.load more')}}</a>
-						@endif
-						
-					</div>
-				</div>
+				@include('isite::frontend.livewire.index.pagination.index')
 			@endif
 	
 		@else
