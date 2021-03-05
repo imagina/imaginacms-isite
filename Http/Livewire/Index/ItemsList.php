442e6c7e64d5f1ce4465aae808fa273151e10e62
Juan Escalante <jcec007@gmail.com>
2021-03-05 17:49:18 -0500

    +Update: added new feature to the itemsList to pass attributes dynamically to the child component

--- Http/Livewire/Index/ItemsList.php
+++ Http/Livewire/Index/ItemsList.php
@@ -10,269 +10,275 @@ use Illuminate\Http\Request;
 
 class ItemsList extends Component
 {
-
-    use WithPagination;
-
-   
-    private $firstRequest;
-
-    public $moduleName;
-    public $repository;
-    public $itemComponentName;
-    public $entityName;
-    public $responsiveTopContent;
-
-    public $totalItems = 0;
-
-    public $search = '';
-
-    private $order;
-    // OrderBy to URL
-    public $orderBy;
-
-    public $configs;
-    public $itemListLayout;
-    public $layoutClass;
-
-    public $wrapperClass;
-    public $pagination;
-    public $showTitle;
-    public $amount;
-
-    public $take;
-
-    public $moduleParams = [];
-    public $filter = [];
-
-    protected $paginationTheme = 'bootstrap';
-    protected $emitItemListRendered;
-
-
-    /**
-    * Listeners
-    */
-    protected $listeners = [
-        'itemsListGetData' => 'getData',
-        'itemsListClearValues' => 'clearValues'
-    ];
-    
-
-    /**
-    * Query String
-    */
-    protected $queryString = [
-        'search' => ['except' => ''],
-        'filter' => ['except' => []],
-        'page' => ['except' => 1]
-    ];
-    
-	/*
+  
+  use WithPagination;
+  
+  
+  private $firstRequest;
+  
+  public $moduleName;
+  public $repository;
+  public $itemComponentName;
+  public $entityName;
+  public $responsiveTopContent;
+  
+  public $totalItems = 0;
+  
+  public $search = '';
+  
+  private $order;
+  // OrderBy to URL
+  public $orderBy;
+  
+  public $configs;
+  public $itemListLayout;
+  public $layoutClass;
+  
+  public $wrapperClass;
+  public $pagination;
+  public $showTitle;
+  public $amount;
+  public $itemComponentAttributes;
+  public $itemComponentNamespace;
+  public $take;
+  
+  public $moduleParams = [];
+  public $filter = [];
+  
+  protected $paginationTheme = 'bootstrap';
+  protected $emitItemListRendered;
+  
+  
+  
+  /**
+   * Listeners
+   */
+  protected $listeners = [
+    'itemsListGetData' => 'getData',
+    'itemsListClearValues' => 'clearValues'
+  ];
+  
+  
+  /**
+   * Query String
+   */
+  protected $queryString = [
+    'search' => ['except' => ''],
+    'filter' => ['except' => []],
+    'page' => ['except' => 1]
+  ];
+  
+  /*
     * Runs once, immediately after the component is instantiated,
     * but before render() is called
     */
-	public function mount($itemListLayout = null, $moduleName = "isite", $entityName = "item", $itemComponentName = "isite::item-list", $params = [] , $responsiveTopContent = null, $showTitle = true, $pagination = null, $configOrderBy = null, $configLayoutIndex = null
-    ){
-
-
-        $this->moduleName = strtolower($moduleName);
-        $this->entityName = strtolower($entityName);
-        $this->itemComponentName = $itemComponentName;
-
-        $this->repository = "Modules\\". ucfirst($this->moduleName) . "\Repositories\\" . ucfirst($entityName).'Repository';
-
-        $this->moduleParams = $params;
-
-        $this->page = $this->moduleParams['page'] ?? 1;
-        $this->take = $this->moduleParams['take'] ?? 12;
-       
-        $this->responsiveTopContent = $responsiveTopContent ?? ["mobile" =>  true, "desktop" => true];
-
-        $this->showTitle = $showTitle;
-      
-        $this->pagination = $pagination ? array_merge(['show' => true , 'type' => 'normal'],$pagination) : ['show' => true , 'type' => 'normal'];
-
-
-        $this->initConfigs($configOrderBy,$configLayoutIndex);
-        $this->initValuesOrderBy();
-        $this->initValuesLayout($itemListLayout);
-        $this->initRequest();
-       
-	}
-
-    /*
-    * Init Configs
-    *
-    */
-    public function initConfigs($configOrderBy,$configLayoutIndex){
-
-        $this->configs['orderBy'] = $configOrderBy ?? config("asgard.isite.config.orderBy");
-        $this->configs['itemListLayout'] = $configLayoutIndex ?? config("asgard.isite.config.layoutIndex");
+  public function mount($itemComponentNamespace="Modules\Isite\View\Components\ItemList", $itemListLayout = null,
+                        $moduleName = "isite", $entityName = "item", $itemComponentName = "isite::item-list",
+                        $params = [] , $responsiveTopContent = null, $showTitle = true, $pagination = null,
+                        $configOrderBy = null, $configLayoutIndex = null, $itemComponentAttributes = []
+  ){
+    
+    $this->moduleName = strtolower($moduleName);
+    $this->entityName = strtolower($entityName);
+    $this->itemComponentName = $itemComponentName;
+    $this->itemComponentAttributes = $itemComponentAttributes;
+    $this->itemComponentNamespace = $itemComponentNamespace;
+    
+    $this->repository = "Modules\\". ucfirst($this->moduleName) . "\Repositories\\" . ucfirst($entityName).'Repository';
+    
+    $this->moduleParams = $params;
+    
+    $this->page = $this->moduleParams['page'] ?? 1;
+    $this->take = $this->moduleParams['take'] ?? 12;
+    
+    $this->responsiveTopContent = $responsiveTopContent ?? ["mobile" =>  true, "desktop" => true];
+    
+    $this->showTitle = $showTitle;
+    
+    $this->pagination = $pagination ? array_merge(['show' => true , 'type' => 'normal'],$pagination) : ['show' => true , 'type' => 'normal'];
+    
+    
+    $this->initConfigs($configOrderBy,$configLayoutIndex);
+    $this->initValuesOrderBy();
+    $this->initValuesLayout($itemListLayout);
+    $this->initRequest();
+    
+  }
+  
+  /*
+  * Init Configs
+  *
+  */
+  public function initConfigs($configOrderBy,$configLayoutIndex){
+    
+    $this->configs['orderBy'] = $configOrderBy ?? config("asgard.isite.config.orderBy");
+    $this->configs['itemListLayout'] = $configLayoutIndex ?? config("asgard.isite.config.layoutIndex");
+  }
+  
+  /*
+  * Init Values Orderby
+  *
+  */
+  public function initValuesOrderBy(){
+    
+    // OrderBy to URL
+    $this->orderBy = $this->configs['orderBy']['default'] ?? "nameaz";
+    $this->order = $this->configs['orderBy']['options'][$this->orderBy]['order'];
+    
+  }
+  
+  /*
+  * Init Values To ChangeLayout
+  *
+  */
+  public function initValuesLayout($itemListLayout){
+    
+    $this->itemListLayout = $itemListLayout ?? $this->configs['itemListLayout']['default'] ?? "four";
+    $this->layoutClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['class'];
+    
+    $this->wrapperClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['wrapperClass'] ?? 'row';
+  }
+  
+  
+  /*
+  * Listener - GET DATA FROM FILTERS
+  *
+  */
+  public function getData($params){
+    
+    //\Log::info("ITEMLIST - GETDATA - PARAMS: ".json_encode($params));
+    
+    if(isset($params["filter"])){
+      $this->emitItemListRendered = true;
+      $this->filter = array_merge($this->filter, $params["filter"]);
+      $this->resetPage();
     }
-
-    /*
-    * Init Values Orderby
-    *
-    */
-    public function initValuesOrderBy(){
-
-        // OrderBy to URL
-        $this->orderBy = $this->configs['orderBy']['default'] ?? "nameaz";
-        $this->order = $this->configs['orderBy']['options'][$this->orderBy]['order'];
-
+    
+    if(isset($params["order"])){
+      $this->emitItemListRendered = false;
+      $this->orderBy = $params['order'];
+      $this->resetPage();
     }
-
+    
     /*
-    * Init Values To ChangeLayout
-    *
-    */
-    public function initValuesLayout($itemListLayout){
-
-        $this->itemListLayout = $itemListLayout ?? $this->configs['itemListLayout']['default'] ?? "four";
-        $this->layoutClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['class'];
-
-        $this->wrapperClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['wrapperClass'] ?? 'row';
-    } 
-
-
-    /*
-    * Listener - GET DATA FROM FILTERS
-    *
-    */
-    public function getData($params){
-
-        //\Log::info("ITEMLIST - GETDATA - PARAMS: ".json_encode($params));
-
-        if(isset($params["filter"])){
-            $this->emitItemListRendered = true;
-            $this->filter = array_merge($this->filter, $params["filter"]);
-            $this->resetPage();
-        }
-        
-        if(isset($params["order"])){
-            $this->emitItemListRendered = false;
-            $this->orderBy = $params['order'];
-            $this->resetPage();
-        }
-
-        /*
-        if(isset($params["layout"])){
-            array_merge($this->layout, $params["layout"]);
-        
-        }
-        */
-
+    if(isset($params["layout"])){
+        array_merge($this->layout, $params["layout"]);
+    
     }
-
-    /*
-    * Init Request
-    *
     */
-    public function initRequest(){
-        $this->firstRequest = true;
-        $this->emitItemListRendered = false;
-        $this->fill(request()->only('search', 'filter','page','orderBy'));
+    
+  }
+  
+  /*
+  * Init Request
+  *
+  */
+  public function initRequest(){
+    $this->firstRequest = true;
+    $this->emitItemListRendered = false;
+    $this->fill(request()->only('search', 'filter','page','orderBy'));
+  }
+  
+  /*
+  * Function Frontend - When change the layout
+  *
+  */
+  public function changeLayout($c){
+    $this->itemListLayout = $c;
+    $this->layoutClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['class'];
+  }
+  
+  /*
+  * Listener
+  * Clear Values
+  */
+  public function clearValues(){
+    //\Log::info($this->name."- CLEAR VALUES FILTER");
+    $this->filter = [];
+    $this->emitItemListRendered = true;
+    
+  }
+  
+  /*
+  * Function Frontend - Load More Button
+  *
+  */
+  public function loadMore(){
+    $this->take += $this->moduleParams['take'];
+  }
+  
+  /*
+ * Make params to Repository
+ * before execcute the query
+ */
+  public function makeParamsToRepository(){
+    
+    if($this->firstRequest)
+      $this->firstRequest = false;
+    
+    // To First Time and Others
+    $this->order = $this->configs['orderBy']['options'][$this->orderBy]['order'];
+    
+    if(is_string($this->search) && $this->search){
+      $this->filter["search"] = $this->search;
+      $this->filter["locale"] = App::getLocale();
     }
-
-    /*
-    * Function Frontend - When change the layout
-    *
-    */
-    public function changeLayout($c){
-        $this->itemListLayout = $c;
-        $this->layoutClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['class'];
+    
+    $params = [
+      "include" => $this->moduleParams['include'] ?? [],
+      "take" => $this->take,
+      "page" => $this->page,
+      "filter" => $this->filter,
+      "order" =>  $this->order
+    ];
+    
+    if(isset($this->moduleParams['filter']) && !empty($this->moduleParams['filter']) ){
+      $params["filter"] = array_merge_recursive($params["filter"], $this->moduleParams['filter']);
     }
-
-    /*
-    * Listener 
-    * Clear Values
-    */
-    public function clearValues(){
-        //\Log::info($this->name."- CLEAR VALUES FILTER");
-        $this->filter = [];
-        $this->emitItemListRendered = true;
-
+    
+    return $params;
+    
+  }
+  
+  /*
+  * Get Item Repository
+  *
+  */
+  private function getItemRepository(){
+    return app($this->repository);
+  }
+  
+  
+  /*
+  * Render
+  *
+  */
+  public function render(){
+    
+    
+    if(!$this->firstRequest && !in_array('orderBy', $this->queryString)){
+      array_push($this->queryString, 'orderBy');
     }
-
-    /*
-    * Function Frontend - Load More Button
-    *
-    */
-    public function loadMore(){
-        $this->take += $this->moduleParams['take'];
-    }
-
-     /*
-    * Make params to Repository
-    * before execcute the query
-    */
-    public function makeParamsToRepository(){
-
-        if($this->firstRequest)
-            $this->firstRequest = false;
-
-        // To First Time and Others
-        $this->order = $this->configs['orderBy']['options'][$this->orderBy]['order'];
-        
-        if(is_string($this->search) && $this->search){
-          $this->filter["search"] = $this->search;
-          $this->filter["locale"] = App::getLocale();
-        }
-
-        $params = [
-            "include" => $this->moduleParams['include'] ?? [],
-            "take" => $this->take,
-            "page" => $this->page,
-            "filter" => $this->filter,
-            "order" =>  $this->order
-        ];
-       
-        if(isset($this->moduleParams['filter']) && !empty($this->moduleParams['filter']) ){
-             $params["filter"] = array_merge_recursive($params["filter"], $this->moduleParams['filter']); 
-        }
-
-        return $params;
-        
-    }
-
-    /*
-    * Get Item Repository
-    *
-    */
-    private function getItemRepository(){
-        return app($this->repository);
-    }
-
-   
-    /*
-    * Render
-    *
-    */
-    public function render(){
-        
-        
-        if(!$this->firstRequest && !in_array('orderBy', $this->queryString)){
-            array_push($this->queryString, 'orderBy');
-        }
-
-        $params = $this->makeParamsToRepository();
-
-        //\Log::info("ITEM LIST - RENDER - PARAMS QUERY ".json_encode($params));
-        
-        $items = $this->getItemRepository()->getItemsBy(json_decode(json_encode($params)));
-
-        $this->totalItems = $items->total();
-
-        $tpl = 'isite::frontend.livewire.index.items-list';
-
-        // Add value name to order on Filter
-        $params ['orderBy'] = $this->orderBy;
-
-        // Emit Finish Render
-        //\Log::info("Emit list rendered: ".json_encode($this->emitItemListRendered));
-        $this->emitItemListRendered ? $this->emit('itemListRendered', $params) : false;
-
-        return view($tpl,['items'=> $items, 'params' => $params]);
-        
-    }
-
+    
+    $params = $this->makeParamsToRepository();
+    
+    //\Log::info("ITEM LIST - RENDER - PARAMS QUERY ".json_encode($params));
+    
+    $items = $this->getItemRepository()->getItemsBy(json_decode(json_encode($params)));
+    
+    $this->totalItems = $items->total();
+    
+    $tpl = 'isite::frontend.livewire.index.items-list';
+    
+    // Add value name to order on Filter
+    $params ['orderBy'] = $this->orderBy;
+    
+    // Emit Finish Render
+    //\Log::info("Emit list rendered: ".json_encode($this->emitItemListRendered));
+    $this->emitItemListRendered ? $this->emit('itemListRendered', $params) : false;
+    
+    return view($tpl,['items'=> $items, 'params' => $params]);
+    
+  }
+  
 }
