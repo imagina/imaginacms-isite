f5c02096d30565f8bd829d107c65b57894ca1012
William Verde <williamverde10@gmail.com>
2021-04-16 15:12:27 -0400

    Added: class primary to preloader all page

--- Resources/views/frontend/partials/preloader-all-page.blade.php
+++ Resources/views/frontend/partials/preloader-all-page.blade.php
@@ -1,7 +1,7 @@
 <div class="loading loading-all-page">
 	<div wire:loading >
 		<div class="capa w-100 h-100 position-fixed d-flex justify-content-center align-items-center">
-			<div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+			<div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
 		  		<span class="sr-only">Loading...</span>
 			</div>
 		</div>
