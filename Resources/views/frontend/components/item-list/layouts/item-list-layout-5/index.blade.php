cab5a2b2941f1ad6406fe6ef52e93441e743fd98
Juan Escalante <jcec007@gmail.com>
2021-04-23 17:58:24 -0500

    +Add: New layouts whatsapp and item List components

---
+++ Resources/views/frontend/components/item-list/layouts/item-list-layout-5/index.blade.php
@@ -0,0 +1,84 @@
+<div class="item-layout item-list-layout-5">
+    <div class="card-item">
+
+        <div class="row row-left h-100 mx-0">
+            <div class="col-md-6 mr-md-auto col-left position-relative h-100 pl-0">
+                <div class="item-image">
+                    <x-media::single-image :alt="$item->title ?? $item->name" :title="$item->title ?? $item->name" :
+                                           :url="$item->url ?? null" :isMedia="true" width="100%"
+                                           :mediaFiles="$item->mediaFiles()" :zone="$mediaImage ?? 'mainimage'"/>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-right ml-auto">
+                    <div class="item-content pl-md-5 pl-lg-4">
+
+                        <div class="{{$orderClasses["title"] ?? 'order-1'}} item-title">
+                            @if(isset($item->url))
+                                <a href="{{$item->url}}">
+                                    @endif
+                                    <h3 class="title">
+                                        {{$item->title ?? $item->name}}
+                                    </h3>
+                                    @if(isset($item->url))
+                                </a>
+                            @endif
+                        </div>
+                        @if($withCreatedDate && isset($item->created_at))
+                            <div class="{{$orderClasses["date"] ?? 'order-2'}} item-created-date">
+                                @if(isset($item->url))
+                                    <a href="{{$item->url}}">
+                                        @endif
+                                        <div class="created-date">{{ $item->created_at->format($formatCreatedDate) }}</div>
+                                        @if(isset($item->url))
+                                    </a>
+                                @endif
+                            </div>
+                        @endif
+                        @if($withCategory && isset($item->category->id))
+                            <div class="{{$orderClasses["categoryTitle"] ?? 'order-3'}} item-category">
+                                @if(isset($item->category->url))
+                                    <a href="{{$item->category->url}}">
+                                        @endif
+                                        <h5 class="category">
+                                            {{$item->category->title ?? $item->category->name}}
+                                        </h5>
+                                        @if(isset($item->category->url))
+                                    </a>
+                                @endif
+                            </div>
+                        @endif
+                        @if($withSummary && ( isset($item->summary) || isset($item->description)) )
+                            <div class="{{$orderClasses["summary"] ?? 'order-4'}} item-summary">
+                                @if(isset($item->url))
+                                    <a href="{{$item->url}}">
+                                        @endif
+                                        <div class="summary">
+                                            {{ Str::limit( $item->summary ?? '', 100) }}
+                                        </div>
+                                        @if(isset($item->url))
+                                    </a>
+                                @endif
+                            </div>
+                        @endif
+                        @if($withViewMoreButton)
+                            <div class="{{$orderClasses["viewMoreButton"] ?? 'order-5'}} item-view-more-button">
+                                @if(isset($item->url))
+                                    <a href="{{$item->url}}" class="btn view-more-button">
+                                        @endif
+                                        {{trans($viewMoreButtonLabel)}}
+                                        @if(isset($item->url))
+                                    </a>
+                                @endif
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
