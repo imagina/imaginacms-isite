372ce96eb7f60e5df51bc272862b44c1f98040d6
Slim Arias Sanchez <slimarias1987@gmail.com>
2021-05-26 17:40:37 -0500

    +FIX: persistent captcha button deactivation if it is disabled

--- Resources/views/frontend/components/captcha.blade.php
+++ Resources/views/frontend/components/captcha.blade.php
@@ -1,19 +1,19 @@
-@if($captchaEnabled)
+@if($captchaEnabled && $captchaEnabled === 1)
     {!! app('icaptcha')->display($params) !!}
+    @section('scripts-owl')
+        @parent
+        <script type="text/javascript" src="https://www.google.com/recaptcha/api.js"></script>
+        <script type="text/javascript">
+          $(function(){
+            disable{{ $formId }}Button();
+          });
+          function enable{{ $formId }}Button(response){
+            if(response)
+              $("#{{ $formId }} input[type=submit], #{{ $formId }} button[type=submit]").removeAttr('disabled');
+          }
+          function disable{{ $formId }}Button(){
+            $("#{{ $formId }} input[type=submit], #{{ $formId }} button[type=submit]").attr('disabled','disabled');
+          }
+        </script>
+    @stop
 @endif
-@section('scripts-owl')
-    @parent
-    <script type="text/javascript" src="https://www.google.com/recaptcha/api.js"></script>
-    <script type="text/javascript">
-      $(function(){
-        disable{{ $formId }}Button();
-      });
-      function enable{{ $formId }}Button(response){
-        if(response)
-          $("#{{ $formId }} input[type=submit], #{{ $formId }} button[type=submit]").removeAttr('disabled');
-      }
-      function disable{{ $formId }}Button(){
-        $("#{{ $formId }} input[type=submit], #{{ $formId }} button[type=submit]").attr('disabled','disabled');
-      }
-    </script>
-@stop
