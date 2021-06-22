1bf7ce8d0b219ffba40f1c05b7fcbe12f52f7848
Slim Arias Sanchez <slimarias1987@gmail.com>
2021-06-22 18:26:59 -0500

    +FIX: fields param not included in items list

--- Http/Livewire/Index/ItemsList.php
+++ Http/Livewire/Index/ItemsList.php
@@ -10,12 +10,12 @@ use Illuminate\Http\Request;
 
 class ItemsList extends Component
 {
-  
+
   use WithPagination;
-  
-  
+
+
   private $firstRequest;
-  
+
   public $title;
   public $description;
   public $moduleName;
@@ -25,19 +25,19 @@ class ItemsList extends Component
   public $responsiveTopContent;
   public $itemModal;
   public $itemMainClass;
-  
+
   public $totalItems = 0;
-  
+
   public $search = '';
-  
+
   private $order;
   // OrderBy to URL
   public $orderBy;
-  
+
   public $configs;
   public $itemListLayout;
   public $layoutClass;
-  
+
   public $wrapperClass;
   public $pagination;
   public $showTitle;
@@ -46,18 +46,18 @@ class ItemsList extends Component
   public $itemComponentNamespace;
   public $carouselAttributes;
   public $take;
-  
+
   public $moduleParams = [];
   public $filter = [];
 
   public $uniqueItemListRendered;
   public $emitItemListRenderedName;
-  
+
   protected $paginationTheme = 'bootstrap';
   protected $emitItemListRendered;
-  
-  
-  
+
+
+
   /**
    * Listeners
    */
@@ -65,8 +65,8 @@ class ItemsList extends Component
     'itemsListGetData' => 'getData',
     'itemsListClearValues' => 'clearValues'
   ];
-  
-  
+
+
   /**
    * Query String
    */
@@ -75,23 +75,23 @@ class ItemsList extends Component
     'filter' => ['except' => []],
     'page' => ['except' => 1]
   ];
-  
+
   /*
     * Runs once, immediately after the component is instantiated,
     * but before render() is called
     */
   public function mount(
-    $itemComponentNamespace="Modules\Isite\View\Components\ItemList", 
-    $itemListLayout = null, 
-    $moduleName = "isite", 
-    $entityName = "item", 
-    $itemComponentName = "isite::item-list", 
-    $params = [] , 
-    $responsiveTopContent = null, 
-    $showTitle = true, 
+    $itemComponentNamespace="Modules\Isite\View\Components\ItemList",
+    $itemListLayout = null,
+    $moduleName = "isite",
+    $entityName = "item",
+    $itemComponentName = "isite::item-list",
+    $params = [] ,
+    $responsiveTopContent = null,
+    $showTitle = true,
     $pagination = null,
-    $configOrderBy = null, 
-    $configLayoutIndex = null, 
+    $configOrderBy = null,
+    $configLayoutIndex = null,
     $itemComponentAttributes = [],
     $itemModal = null,
     $carouselAttributes = null,
@@ -99,14 +99,14 @@ class ItemsList extends Component
     $title = null,
     $description = null
   ){
-  
-    
+
+
     $this->moduleName = strtolower($moduleName);
     $this->entityName = strtolower($entityName);
-  
+
     $customIndexTitle = setting($moduleName.'::customIndexTitle');
     $this->title = !empty($title) ? $title : (!empty($customIndexTitle) ? $customIndexTitle : trans($this->moduleName.'::frontend.index.title') );
-  
+
     $customIndexDescription = setting($moduleName.'::customIndexDescription');
     $this->description = !empty($description) ? $description : (!empty($customIndexDescription) ? $customIndexDescription : "");
 
@@ -114,25 +114,25 @@ class ItemsList extends Component
     $this->itemComponentAttributes = $itemComponentAttributes;
     $this->itemComponentNamespace = $itemComponentNamespace;
     $this->itemMainClass = $this->entityName == "ad" ? "pin" : $this->entityName;
-    
+
     $this->repository = "Modules\\". ucfirst($this->moduleName) . "\Repositories\\" . ucfirst($entityName).'Repository';
-    
+
     $this->moduleParams = $params;
-    
+
     $this->page = $this->moduleParams['page'] ?? 1;
     $this->take = $this->moduleParams['take'] ?? 12;
-    
+
     $this->responsiveTopContent = $responsiveTopContent ?? ["mobile" =>  true, "desktop" => true];
-    
+
     $this->showTitle = $showTitle;
-    
+
     $this->pagination = $pagination ? array_merge(['show' => true , 'type' => 'normal'],$pagination) : ['show' => true , 'type' => 'normal'];
 
     $this->itemModal = $itemModal ?? ["mobile" =>  false, "desktop" => false, "idModal" => 'modal_'.$this->id];
     $this->carouselAttributes = $carouselAttributes;
 
     $this->uniqueItemListRendered = $uniqueItemListRendered;
-    
+
     $this->initConfigs($configOrderBy,$configLayoutIndex);
     $this->initValuesOrderBy();
     $this->initValuesLayout($itemListLayout);
@@ -146,58 +146,58 @@ class ItemsList extends Component
   *
   */
   public function initConfigs($configOrderBy,$configLayoutIndex){
-    
+
     $this->configs['orderBy'] = $configOrderBy ?? config("asgard.isite.config.orderBy");
     $this->configs['itemListLayout'] = $configLayoutIndex ?? config("asgard.isite.config.layoutIndex");
   }
-  
+
   /*
   * Init Values Orderby
   *
   */
   public function initValuesOrderBy(){
-    
+
     // OrderBy to URL
     $this->orderBy = $this->configs['orderBy']['default'] ?? "nameaz";
     $this->order = $this->configs['orderBy']['options'][$this->orderBy]['order'];
-    
+
   }
-  
+
   /*
   * Init Values To ChangeLayout
   *
   */
   public function initValuesLayout($itemListLayout){
-    
+
     $this->itemListLayout = $itemListLayout ?? $this->configs['itemListLayout']['default'] ?? "four";
     $this->layoutClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['class'];
-    
+
     $this->wrapperClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['wrapperClass'] ?? 'row';
   }
-  
-  
+
+
   /*
   * Listener - GET DATA FROM FILTERS
   *
   */
   public function getData($params){
-    
+
     //\Log::info("ITEMLIST - GETDATA - PARAMS: ".json_encode($params));
-    
+
     if(isset($params["filter"])){
       $this->emitItemListRendered = true;
       $this->filter = array_merge($this->filter, $params["filter"]);
       $this->resetPage();
     }
-    
+
     if(isset($params["order"])){
       $this->emitItemListRendered = false;
       $this->orderBy = $params['order'];
       $this->resetPage();
     }
-    
+
   }
-  
+
   /*
   * Init Request
   *
@@ -207,7 +207,7 @@ class ItemsList extends Component
     $this->emitItemListRendered = false;
     $this->fill(request()->only('search', 'filter','page','orderBy'));
   }
-  
+
   /*
   * Function Frontend - When change the layout
   *
@@ -216,7 +216,7 @@ class ItemsList extends Component
     $this->itemListLayout = $c;
     $this->layoutClass = $this->configs['itemListLayout']['options'][$this->itemListLayout]['class'];
   }
-  
+
   /*
   * Listener
   * Clear Values
@@ -225,9 +225,9 @@ class ItemsList extends Component
     //\Log::info($this->name."- CLEAR VALUES FILTER");
     $this->filter = [];
     $this->emitItemListRendered = true;
-    
+
   }
-  
+
   /*
   * Function Frontend - Load More Button
   *
@@ -236,40 +236,41 @@ class ItemsList extends Component
     $this->take += $this->moduleParams['take'];
   }
 
-  
+
   /*
  * Make params to Repository
  * before execcute the query
  */
   public function makeParamsToRepository(){
-    
+
     if($this->firstRequest)
       $this->firstRequest = false;
-    
+
     // To First Time and Others
     $this->order = $this->configs['orderBy']['options'][$this->orderBy]['order'];
-    
+
     if(is_string($this->search) && $this->search){
       $this->filter["search"] = $this->search;
       $this->filter["locale"] = App::getLocale();
     }
-    
+
     $params = [
       "include" => $this->moduleParams['include'] ?? [],
       "take" => $this->take,
       "page" => $this->page,
       "filter" => $this->filter,
-      "order" =>  $this->order
+      "order" =>  $this->order,
+      "fields" =>  $this->moduleParams['fields'] ?? [],
     ];
-    
+
     if(isset($this->moduleParams['filter']) && !empty($this->moduleParams['filter']) ){
       $params["filter"] = array_merge_recursive($params["filter"], $this->moduleParams['filter']);
     }
-    
+
     return $params;
-    
+
   }
-  
+
   /*
   * Get Item Repository
   *
@@ -277,8 +278,8 @@ class ItemsList extends Component
   private function getItemRepository(){
     return app($this->repository);
   }
-  
-  
+
+
   /*
   * Validate Name Emit List Rendered
   * To Frontend multiples components
@@ -295,24 +296,24 @@ class ItemsList extends Component
   *
   */
   public function render(){
-    
-    
+
+
     if(!$this->firstRequest && !in_array('orderBy', $this->queryString)){
       array_push($this->queryString, 'orderBy');
     }
-    
+
     $params = $this->makeParamsToRepository();
-    
+
     //\Log::info("ITEM LIST - RENDER - PARAMS QUERY ".json_encode($params));
-    
+
     $items = $this->getItemRepository()->getItemsBy(json_decode(json_encode($params)));
-    
+
     $this->totalItems = $items->total();
 
     //\Log::info("ITEM LIST - TOTAL: ".$items->total());
-    
+
     $tpl = 'isite::frontend.livewire.index.items-list';
-    
+
     // Add value name to order on Filter
     $params ['orderBy'] = $this->orderBy;
 
@@ -325,9 +326,9 @@ class ItemsList extends Component
     // Emit Finish Render
     //\Log::info("Emit list rendered: ".json_encode($this->emitItemListRendered));
     $this->emitItemListRendered ? $this->emit($this->emitItemListRenderedName, $params) : false;
-    
+
     return view($tpl,['items'=> $items, 'params' => $params]);
-    
+
   }
-  
+
 }
