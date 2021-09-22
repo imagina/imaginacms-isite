ab130c75d519ce410a4b19375d872d0565dae5df
T-JULIAND <juliandavidbonillatorres@gmail.com>
2021-09-22 18:00:51 -0500

    +fix | Front-end : Validate show button "edit-link"

--- Resources/views/frontend/components/whatsapp/layouts/whatsapp-layout-2/index.blade.php
+++ Resources/views/frontend/components/whatsapp/layouts/whatsapp-layout-2/index.blade.php
@@ -1,64 +1,64 @@
 <!--Validation to show icon or dropdown -->
-@if($editButton == true)
-  @if(count($items) <= 1)
+@if(count($items) <= 1)
+  @if($editButton == true)
     <x-isite::edit-link
-      link="/iadmin/#/site/settings?module=isite&settings=whatsapp1,whatsapp2,whatsapp3"
-      :tooltip="trans('isite::common.editLink.tooltipWhatsapp')"/>
-    @foreach($items as $key => $item)
-      <a href="https://wa.me/{{ $item->callingCode }}{{ $item->number }}?text={{ $item->message }}"
-         class="whatsapp-layout-2" target="_blank">
-        @if($type)
-          <span class="fa-stack fa-{{ $size }}">
-          <i class="fa fa-{{ $type }} fa-stack-2x"></i>
-          <i
-            class="icon-whatsapp {{ $icon }} fa-stack-1x @if($type=='square-o' || $type=='circle-thin' || empty($type))  @else text-white @endif"></i>
-        </span>
-        @else
-          <i class="icon-whatsapp {{ $icon }} fa-{{ $size }}"></i>
-        @endif
-      </a>
-    @endforeach
-  @else
-    <x-isite::edit-link
-      link="/iadmin/#/site/settings?module=isite&settings=whatsapp1,whatsapp2,whatsapp3"
-      :tooltip="trans('isite::common.editLink.tooltipWhatsapp')"/>
-    <div class="btn-group {{$alignment}} whatsapp-layout-2">
-      <a id="dropdownMenuWhatsapp" type="button"
-         class="btn dropdown-toggle{{ count($parentAttributes) > 0 ? ' p-0' : '' }}" data-toggle="dropdown"
-         aria-haspopup="true" aria-expanded="false">
-        @if($type)
-          <span class="fa-stack fa-{{ $size }}">
-          <i class="fa fa-{{ $type }} fa-stack-2x"></i>
-          <i
-            class="icon-whatsapp {{ $icon }} fa-stack-1x @if($type=='square-o' || $type=='circle-thin' || empty($type))  @else text-white @endif"></i>
-        </span>
-        @else
-          <i class="icon-whatsapp {{ $icon }} fa-{{ $size }}"></i>
-        @endif
-      </a>
-      <div class="dropdown-menu dropdown-menu-whatsapp">
-        <!-- Dropdown menu links -->
-        @foreach($items as $key => $item)
-          @if(!empty($item->callingCode) && !empty($item->number))
-            <div class="number-whatsapp">
-              <a href="https://wa.me/{{ $item->callingCode }}{{ $item->number }}?text={{ $item->message }}"
-                 target="_blank">
-                <p class="mb-0">
-                  @if(isset($item->iconLabel) && !empty($item->iconLabel))
-                    <i class="{{ $item->iconLabel}} "></i>
-                  @endif
-                  <span>{{ isset($item->label) ? $item->label.'' :'' }}</span>
-                </p>
-                @if($notNumber == true)
-                  <p class="mb-0 formatted-number">
-                    <span>{{ $item->formattedNumber }}</span>
-                  </p>
-                @endif
-              </a>
-            </div>
-          @endif
-        @endforeach
-      </div>
-    </div>
+            link="/iadmin/#/site/settings?module=isite&settings=whatsapp1,whatsapp2,whatsapp3"
+            :tooltip="trans('isite::common.editLink.tooltipWhatsapp')"/>
   @endif
+  @foreach($items as $key => $item)
+    <a href="https://wa.me/{{ $item->callingCode }}{{ $item->number }}?text={{ $item->message }}"
+       class="whatsapp-layout-2" target="_blank">
+      @if($type)
+        <span class="fa-stack fa-{{ $size }}">
+          <i class="fa fa-{{ $type }} fa-stack-2x"></i>
+          <i
+                  class="icon-whatsapp {{ $icon }} fa-stack-1x @if($type=='square-o' || $type=='circle-thin' || empty($type))  @else text-white @endif"></i>
+        </span>
+      @else
+        <i class="icon-whatsapp {{ $icon }} fa-{{ $size }}"></i>
+      @endif
+    </a>
+  @endforeach
+@else
+  <x-isite::edit-link
+          link="/iadmin/#/site/settings?module=isite&settings=whatsapp1,whatsapp2,whatsapp3"
+          :tooltip="trans('isite::common.editLink.tooltipWhatsapp')"/>
+  <div class="btn-group {{$alignment}} whatsapp-layout-2">
+    <a id="dropdownMenuWhatsapp" type="button"
+       class="btn dropdown-toggle{{ count($parentAttributes) > 0 ? ' p-0' : '' }}" data-toggle="dropdown"
+       aria-haspopup="true" aria-expanded="false">
+      @if($type)
+        <span class="fa-stack fa-{{ $size }}">
+          <i class="fa fa-{{ $type }} fa-stack-2x"></i>
+          <i
+                  class="icon-whatsapp {{ $icon }} fa-stack-1x @if($type=='square-o' || $type=='circle-thin' || empty($type))  @else text-white @endif"></i>
+        </span>
+      @else
+        <i class="icon-whatsapp {{ $icon }} fa-{{ $size }}"></i>
+      @endif
+    </a>
+    <div class="dropdown-menu dropdown-menu-whatsapp">
+      <!-- Dropdown menu links -->
+      @foreach($items as $key => $item)
+        @if(!empty($item->callingCode) && !empty($item->number))
+          <div class="number-whatsapp">
+            <a href="https://wa.me/{{ $item->callingCode }}{{ $item->number }}?text={{ $item->message }}"
+               target="_blank">
+              <p class="mb-0">
+                @if(isset($item->iconLabel) && !empty($item->iconLabel))
+                  <i class="{{ $item->iconLabel}} "></i>
+                @endif
+                <span>{{ isset($item->label) ? $item->label.'' :'' }}</span>
+              </p>
+              @if($notNumber == true)
+                <p class="mb-0 formatted-number">
+                  <span>{{ $item->formattedNumber }}</span>
+                </p>
+              @endif
+            </a>
+          </div>
+        @endif
+      @endforeach
+    </div>
+  </div>
 @endif
