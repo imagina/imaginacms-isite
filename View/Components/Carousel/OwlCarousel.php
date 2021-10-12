eeae9b6bf0a2016178c0ace264cd9393291b499f
T-JULIAND <juliandavidbonillatorres@gmail.com>
2021-10-12 18:39:43 -0500

    +fix | Front-end : Validate another view carousel

--- View/Components/Carousel/OwlCarousel.php
+++ View/Components/Carousel/OwlCarousel.php
@@ -39,7 +39,7 @@ class OwlCarousel extends Component
    *
    * @return void
    */
-  public function __construct($repository, $id, $params = [], $margin = 10, $responsiveClass = true, $autoplay = true,
+  public function __construct($repository, $id, $view = null, $params = [], $margin = 10, $responsiveClass = true, $autoplay = true,
                               $autoplayHoverPause = true, $loop = true, $dots = true, $nav = true, $center = false, $responsive = null,
                               $itemLayout = null, $title = "", $subTitle = "", $itemsBySlide = 1, $navText = "",
                               $containerFluid = false, $itemComponent = null, $owlBlockStyle = null)
@@ -66,7 +66,7 @@ class OwlCarousel extends Component
     $this->containerFluid = $containerFluid;
     $this->owlBlockStyle = $owlBlockStyle;
     $this->itemComponent = $itemComponent ?? "isite::item-list";
-    $this->view = "isite::frontend.components.owl.carousel";
+    $this->view = $view ?? "isite::frontend.components.owl.carousel";
     $this->getItems();
 
     list($this->editLink, $this->tooltipEditLink) = getEditLink($this->repository);
