2804150020afae5015a4ced8c11407ad024fc864
Cristian Marin <marin101998@gmail.com>
2022-02-03 12:15:42 -0500

    +fix | filter select new params

--- Resources/views/frontend/livewire/filters/select/layouts/select-layout-1/index.blade.php
+++ Resources/views/frontend/livewire/filters/select/layouts/select-layout-1/index.blade.php
@@ -1,31 +1,34 @@
 <div class="filter-{{$type}} filter-{{$type}}-layout-{{$layout}} filter-{{$name}}">
   @if($status)
-	<div class="title">
-      <a class="item mb-3" @if($isCollapsable) data-toggle="collapse" href="#collapse-{{$name}}" role="button"
-         aria-expanded="{{$isExpanded ? 'true' : 'false'}}" aria-controls="collapse-{{$name}}"
-         class="{{$isExpanded ? '' : 'collapsed'}}" @endif>
-            <h5 class="p-3 border-top border-bottom">
-	  			<i class="fa angle float-right" aria-hidden="true"></i>
-	  			{{$title}}
-	  		</h5>
-	  		
-		</a>
-	</div>
+    @if($withTitle)
+      <div class="title">
+        <a class="item mb-3" @if($isCollapsable) data-toggle="collapse" href="#collapse-{{$name}}" role="button"
+           aria-expanded="{{$isExpanded ? 'true' : 'false'}}" aria-controls="collapse-{{$name}}"
+           class="{{$isExpanded ? '' : 'collapsed'}}" @endif>
+          <h5 class="p-3 border-top border-bottom">
+            <i class="fa angle float-right" aria-hidden="true"></i>
+            {{$title}}
+          </h5>
+        </a>
+      </div>
+    @endif
 
-	<div class="content position-relative m-3">
+    <div class="content position-relative m-3">
 
-		@include('isite::frontend.partials.preloader')
+      @include('isite::frontend.partials.preloader')
       <div @if($isCollapsable) class="collapse multi-collapse {{$isExpanded ? 'show' : ''}}"
            id="collapse-{{$name}}" @endif>
-				<label>{{$title}}</label>
+        @if($withSubtitle)
+          <label>{{$title}}</label>
+        @endif
         <select class="form-control" name="select" wire:model="selected">
-					<option value="NULL">Seleccione un(a) {{$title}}</option>
+          <option value="NULL">Seleccione un(a) {{$title}}</option>
           @foreach($options as $item)
             <option value="{{$item->id}}">{{$item->title ?? $item->name}}</option>
           @endforeach
         </select>
-		</div>
+      </div>
 
-	</div>
-@endif
+    </div>
+  @endif
 </div>
