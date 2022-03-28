c8defe7238fe53960c30f50d33a4f312babee639
Juan Escalante <jcec007@gmail.com>
2022-03-28 14:53:55 -0500

    +Add: New Buttons Blade component | made it by Adriana Cardoso

--- Resources/views/frontend/components/button.blade.php
+++ Resources/views/frontend/components/button.blade.php
@@ -1,10 +1,22 @@
-<a class="btn btn-sm btn{{Str::contains($style, 'outline') ? "-outline" : ""}}-primary {{$buttonClasses}}"
-   {{ !empty($onclick) ? "onclick=".$onclick : "" }}>
-  @if($withIcon)
+@if($style=="")
+    @php $var="text-".$color @endphp
+@else
+    @php
+        $var1 = str_contains($style,'outline') ? "-outline-" : "-";
+        $var= "button".$var1.$color;
+    @endphp
+@endif
+<a class="button-base {{$var}} {{$buttonClasses}}"
+   {{ !empty($onclick) ? "onclick=".$onclick : "" }} {{ !empty($href) ? "href=".$href : "" }}
+        {{ !empty($target) ? "target=".$target : "" }} {{ !empty($dataPid) ? "data-item-id=".$dataPid : "" }} style="font-size: {{$sizeLabel}}px; line-height: {{$sizeLabel}}px;">
+  @if($withIcon && $iconPosition=="left")
     <i class="{{$iconClass}}"></i>
   @endif
   @if($withLabel)
-    {{$label}}
+    <span> {{$label}} </span>
+  @endif
+  @if($withIcon && $iconPosition=="right")
+      <i class="{{$iconClass}}"></i>
   @endif
 </a>
 
