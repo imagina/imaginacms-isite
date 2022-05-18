b099cdb95e7cb70597ec32c62982b957f4396f7d
msolano <msolano@imaginacolombia.com>
2022-05-17 19:32:35 -0500

    +Add: Endpoint (API) to get modules info

--- Http/ApiRoutes/configsRoutes.php
+++ Http/ApiRoutes/configsRoutes.php
@@ -6,5 +6,8 @@ $router->group(['prefix' => '/configs'], function (Router $router) {
   $router->get('/', [
     'uses' => 'ConfigsApiController@index'
   ]);
+  $router->get('/modules-info', [
+    'uses' => 'ConfigsApiController@modulesInfo'
+  ]);
 });
 
