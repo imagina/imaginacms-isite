87162769066eee3eb9345916709710ecec5ec5de
T-JULIAND <juliandavidbonillatorres@gmail.com>
2022-07-13 18:28:55 -0500

    +Fix: Front-End | view component layout-3

--- Resources/views/frontend/components/multilang/layouts/multilang-layout-3/index.blade.php
+++ Resources/views/frontend/components/multilang/layouts/multilang-layout-3/index.blade.php
@@ -1,78 +1,105 @@
 <div id="multiLangLayout3" class="multi-lang">
-  
   <div class="dropdown">
-    <?php
-    $hash = sha1($butonComponentNamespace);
-    if (isset($component)) {
-      $__componentOriginal{$hash} = $component;
-    }
-    $component = $__env->getContainer()->make($butonComponentNamespace, array_merge([
-      'idButton' => 'buttonDropdownMultilang',
-      'label' => $longText ? config('available-locales')[LaravelLocalization::getCurrentLocale()]['native'] : LaravelLocalization::getCurrentLocale(),
-      'withLabel' => true,
-      'buttonClasses' => 'btn btn-sm dropdown-toggle text-capitalize'
-    ], $buttonComponentAtributtes ?? []));
-    $component->withName($butonComponent);
-    if ($component->shouldRender()):
-      $__env->startComponent($component->resolveView(), $component->data());
-      if (isset($__componentOriginal{$hash})):
-        $component = $__componentOriginal{$hash};
-        unset($__componentOriginal{$hash});
+    @if($showButton)
+      <?php
+      $hash = sha1($butonComponentNamespace);
+      if (isset($component)) {
+        $__componentOriginal{$hash} = $component;
+      }
+      $component = $__env->getContainer()->make($butonComponentNamespace, array_merge([
+        'idButton' => 'buttonDropdownMultilang',
+        'label' => $longText ? config('available-locales')[LaravelLocalization::getCurrentLocale()]['native'] : LaravelLocalization::getCurrentLocale(),
+        'withLabel' => true,
+        'buttonClasses' => 'btn btn-sm dropdown-toggle border-0 text-capitalize text-bold',
+      ], $buttonComponentAtributtes ?? []));
+
+      $component->withName($butonComponent);
+      if ($component->shouldRender()):
+        $__env->startComponent($component->resolveView(), $component->data());
+        if (isset($__componentOriginal{$hash})):
+          $component = $__componentOriginal{$hash};
+          unset($__componentOriginal{$hash});
+        endif;
+        echo $__env->renderComponent();
       endif;
-      echo $__env->renderComponent();
-    endif;
-    ?>
+      ?>
+    @endif
+    @if($showImage)
+      <?php
+      $hash = sha1($imageComponentNamespace);
+      if (isset($component)) {
+        $__componentOriginal{$hash} = $component;
+      }
+      $component = $__env->getContainer()->make($imageComponentNamespace, array_merge([
+        'src' => url('modules/isite/img/locales/' . LaravelLocalization::getCurrentLocale() . '.jpg'),
+        'imgStyles' => '  width: 35px; height: 33px; object-fit: cover;',
+        'imgClasses' => 'rounded-circle mx-2',
+        'url' => LaravelLocalization::getCurrentLocale(),
+      ], $imageComponentAtributtes ?? []));
+      $component->withName($imageComponent);
+      if ($component->shouldRender()):
+        $__env->startComponent($component->resolveView(), $component->data());
+        if (isset($__componentOriginal{$hash})):
+          $component = $__componentOriginal{$hash};
+          unset($__componentOriginal{$hash});
+        endif;
+        echo $__env->renderComponent();
+      endif;
+      ?>
+    @endif
     
-    <div class="dropdown-menu dropdown-menu-right  p-2" aria-labelledby="{{$component->idButton}}">
+    <div class="dropdown-menu dropdown-menu-right py-0" aria-labelledby="{{$component->idButton}}">
       @foreach($locales as $locale)
-        @if($showImage)
-          <?php
-          $hash = sha1($imageComponentNamespace);
-          if (isset($component)) {
-            $__componentOriginal{$hash} = $component;
-          }
-          $component = $__env->getContainer()->make($imageComponentNamespace, array_merge([
-            'src' => url('modules/isite/img/locales/' . $locale . '.jpg'),
-            'imgStyles' => 'width: 35px; height: 30px; object-fit:cover',
-            'imgClasses' =>'rounded-circle mx-2'
-          ], $imageComponentAtributtes ?? []));
-          $component->withName($butonComponent);
-          if ($component->shouldRender()):
-            $__env->startComponent($component->resolveView(), $component->data());
-            if (isset($__componentOriginal{$hash})):
-              $component = $__componentOriginal{$hash};
-              unset($__componentOriginal{$hash});
+        <div class="item-dropdown py-3">
+          @if($showImage)
+            <?php
+            $hash = sha1($imageComponentNamespace);
+            if (isset($component)) {
+              $__componentOriginal{$hash} = $component;
+            }
+            $component = $__env->getContainer()->make($imageComponentNamespace, array_merge([
+              'src' => url('modules/isite/img/locales/' . $locale . '.jpg'),
+              'imgStyles' => 'width: 30px; height: 30px; object-fit:cover',
+              'imgClasses' => 'rounded-circle  ml-3 mr-2',
+              'url' => $locale,
+            ], $imageComponentAtributtes ?? []));
+            $component->withName($imageComponent);
+            if ($component->shouldRender()):
+              $__env->startComponent($component->resolveView(), $component->data());
+              if (isset($__componentOriginal{$hash})):
+                $component = $__componentOriginal{$hash};
+                unset($__componentOriginal{$hash});
+              endif;
+              echo $__env->renderComponent();
             endif;
-            echo $__env->renderComponent();
-          endif;
-          ?>
-        @endif
-  
-      @if($showButton)
-          <?php
-          $hash = sha1($butonComponentNamespace);
-          if (isset($component)) {
-            $__componentOriginal{$hash} = $component;
-          }
-          $component = $__env->getContainer()->make($butonComponentNamespace, array_merge([
-            'label' => $longText ? config('available-locales')[$locale]['native'] : $locale,
-            'href' => $locale,
-            'withLabel' => true,
-            'buttonClasses' => 'text-white text-left btn-lg border-0 text-capitalize',
-            'style' => 'outline'
-          ], $buttonDropDownItemComponentAtributtes ?? []));
-          $component->withName($butonComponent);
-          if ($component->shouldRender()):
-            $__env->startComponent($component->resolveView(), $component->data());
-            if (isset($__componentOriginal{$hash})):
-              $component = $__componentOriginal{$hash};
-              unset($__componentOriginal{$hash});
+            ?>
+          @endif
+          
+          @if($showButton)
+            <?php
+            $hash = sha1($butonComponentNamespace);
+            if (isset($component)) {
+              $__componentOriginal{$hash} = $component;
+            }
+            $component = $__env->getContainer()->make($butonComponentNamespace, array_merge([
+              'label' => $longTextDrop ? config('available-locales')[$locale]['native'] : $locale,
+              'href' => $locale,
+              'withLabel' => true,
+              'buttonClasses' => 'text-white text-left btn-lg border-0 text-capitalize text-bold',
+              'style' => 'outline'
+            ], $buttonDropDownItemComponentAtributtes ?? []));
+            $component->withName($butonComponent);
+            if ($component->shouldRender()):
+              $__env->startComponent($component->resolveView(), $component->data());
+              if (isset($__componentOriginal{$hash})):
+                $component = $__componentOriginal{$hash};
+                unset($__componentOriginal{$hash});
+              endif;
+              echo $__env->renderComponent();
             endif;
-            echo $__env->renderComponent();
-          endif;
-          ?>
-        @endif
-        
+            ?>
+          @endif
+        </div>
       @endforeach
     </div>
   </div>
