7ccf68bd64db4c074d23f64d0f7c4c8f63a7726f
William Verde <williamverde10@gmail.com>
2023-02-21 12:16:19 -0400

    Update: Organization Api Controller to last standard

--- Http/Controllers/Api/OrganizationApiController.php
+++ Http/Controllers/Api/OrganizationApiController.php
@@ -3,66 +3,18 @@
 namespace Modules\Isite\Http\Controllers\Api;
 
 use Modules\Core\Icrud\Controllers\BaseCrudController;
-//Model Repository
+//Model
 use Modules\Isite\Entities\Organization;
 use Modules\Isite\Repositories\OrganizationRepository;
-//Model Requests
-use Modules\Isite\Http\Requests\CreateOrganizationRequest;
-use Modules\Isite\Http\Requests\UpdateOrganizationRequest;
-//Transformer
-use Modules\Isite\Transformers\OrganizationTransformer;
 
 class OrganizationApiController extends BaseCrudController
 {
+  public $model;
   public $modelRepository;
 
-  public function __construct(OrganizationRepository $modelRepository)
+  public function __construct(Organization $model, OrganizationRepository $modelRepository)
   {
+    $this->model = $model;
     $this->modelRepository = $modelRepository;
-    
-  }
-  
-  /**
-   * Return request to create model
-   *
-   * @param $modelData
-   * @return false
-   */
-  public function modelCreateRequest($modelData)
-  {
-    return new CreateOrganizationRequest($modelData);
-  }
-
-  /**
-   * Return request to create model
-   *
-   * @param $modelData
-   * @return false
-   */
-  public function modelUpdateRequest($modelData)
-  {
-    return new UpdateOrganizationRequest($modelData);
-  }
-
-  /**
-   * Return model collection transformer
-   *
-   * @param $data
-   * @return mixed
-   */
-  public function modelCollectionTransformer($data)
-  {
-    return OrganizationTransformer::collection($data);
-  }
-
-  /**
-   * Return model transformer
-   *
-   * @param $data
-   * @return mixed
-   */
-  public function modelTransformer($data)
-  {
-    return new OrganizationTransformer($data);
   }
 }
