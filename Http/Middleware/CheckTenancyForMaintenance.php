797a2d36afdd093b4166ea6530565e2a580ed918
William Verde <williamverde10@gmail.com>
2023-02-22 14:51:24 -0400

    Added: Process to maintenance mode

---
+++ Http/Middleware/CheckTenancyForMaintenance.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\Isite\Http\Middleware;
+
+use Closure;
+use Illuminate\Foundation\Http\Exceptions\MaintenanceModeException;
+use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
+use Stancl\Tenancy\Exceptions\TenancyNotInitializedException;
+use Symfony\Component\HttpFoundation\IpUtils;
+
+class CheckTenancyForMaintenance extends CheckForMaintenanceMode
+{
+    public function handle($request, Closure $next)
+    {
+
+        if (! tenant()) {
+            throw new TenancyNotInitializedException;
+        }
+
+        if (tenant('maintenance_mode')) {
+            $data = tenant('maintenance_mode');
+
+            $tpl = 'isite::frontend.errors.maintenance';
+            $ttpl = 'errors.maintenance';
+
+            if (view()->exists($ttpl)) $tpl = $ttpl;
+
+            return view($tpl);
+
+        }
+
+        return $next($request);
+    }
+}
