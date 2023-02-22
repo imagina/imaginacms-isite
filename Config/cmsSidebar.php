909d36f991ab06599345533babe5b08fc461db6c
imagina <msolano2505@gmail.com>
2023-02-22 18:40:23 -0500

    +Add: Menu to first level for organizations

--- Config/cmsSidebar.php
+++ Config/cmsSidebar.php
@@ -7,12 +7,13 @@ return [
       "title" => "isite.cms.sidebar.adminGroup",
       "icon" => "fas fa-chess-rook",
       "children" => [
-        "isite_cms_admin_organizations",
         "isite_cms_admin_categories",
         "isite_cms_admin_icruds",
         "isite_cms_admin_layouts",
       ]
     ],
+    "isite_cms_admin_organizationsAll",
+    "isite_cms_admin_organizations",
     "isite_cms_admin_index"
   ],
   "panel" => [
