f1e9f9d26e593a463501b39ce986144e5aafb68b
Cristian Marin <marin101998@gmail.com>
2023-06-05 10:45:38 -0500

    +fix | new params copyright component

--- Resources/views/frontend/components/copyright/layouts/copyright-layout-2/index.blade.php
+++
@@ -1,3 +0,0 @@
-<p class="copyright">
-  <i class="fa-regular fa-copyright"></i>{{' '.$name.trans('isite::copyright.text')}}
-</p>
--- View/Components/Copyright.php
+++ View/Components/Copyright.php
@@ -10,19 +10,29 @@ class Copyright extends Component
 {
   public $year;
   public $name;
-  public $layout;
+  public $withIconCopyright;
+  public $withLabelCopyright;
+  public $withYear;
+  public $withTitleCopyright;
+  public $withSiteName;
 
   /**
    * Create a new component instance.
    *
    * @return void
    */
-  public function __construct($classes = null, $layout = 'copyright-layout-1')
+  public function __construct($classes = null, $withIconCopyright = false, $withLabelCopyright = true,
+                              $withYear = true, $withTitleCopyright = true, $withSiteName = true)
   {
     $this->name = @setting('core::site-name');
     $this->year = date('Y');
-    $this->layout = $layout;
+    $this->withYear = $withYear;
+    $this->withIconCopyright = $withIconCopyright;
+    $this->withLabelCopyright = $withLabelCopyright;
+    $this->withTitleCopyright = $withTitleCopyright;
+    $this->withSiteName = $withSiteName;
   }
+
   /**
    * Get the view / contents that represent the component.
    *
@@ -31,8 +41,6 @@ class Copyright extends Component
   public
   function render()
   {
-    return view("isite::frontend.components.copyright.layouts.".$this->layout.".index");
+    return view("isite::frontend.components.copyright");
   }
 }
-
-
