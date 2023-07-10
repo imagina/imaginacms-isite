a48ae8c21885f29add3328ecf10ef2c249025e81
William Verde <williamverde10@gmail.com>
2023-07-10 18:31:26 -0400

    Update: calls to tenancy services ai

--- Services/TenantAiService.php
+++ Services/TenantAiService.php
@@ -35,76 +35,15 @@ class TenantAiService
     $category = tenant()->category->title ?? null;
     \Log::info($this->log."processAi|Tenant|Id: ".tenant()->id."|Category: ".$category);
 
-    //============================ Pages
-    
-    //$pages = $this->getInforPages();
-
-    //Solo las paginas del sitio (home,us,contact)
+    //Ai Service in Modules
     //$dataPages = $this->pageService->getPages();
-    //dd($dataPages);
-
-    //TODO - Luego toca actualizar paginas
-
-    //============================ Blog
-    /**
-     * Buscar alguna categoria , si ya existe para asociar los posts
-     * o la informacion de la categoria ya viene en la data?
-    */
     //$dataPosts = $this->blogService->getPosts();
-    //dd($dataPosts);
-
-    //TODO - Luego toca CREAR posts
-
-    //============================ Slider
-    /**
-     * El slider principal ya existe
-     * Se tendria que buscar si existe un slider para asociar los slides luego
-    */
     //$dataSlides = $this->sliderService->getSlides();
-    //dd($dataSlides);
-
-    //TODO - Luego toca CREAR slides
-
-    //============================ Icommerce
-    /** 
-     * la informacion de la categoria ya viene en la data?
-    */
     //$dataProducts = $this->icommerceService->getProducts();
-    //dd($dataSProducts);
-
-    //TODO - Luego toca CREAR productos
-
-        
+   
     \Log::info($this->log."processAi|END");
 
   }
-
-
-  /**
-   * OJO
-   */
-  public function getInforPages()
-  {
-
-        \Log::info($this->log."getInforPages");
-      
-        $pages = app("Modules\Page\Repositories\PageRepository")->getItemsBy();
-
-        $prompt = [];
-        foreach ($pages as $key => $page) {
-            \Log::info($this->log."PageTitle: ".$page->title);
-            $data =  [
-                'id' => $page->id,
-                'title' => $page->title
-            ];
-            
-            array_push($prompt,$page->title);
-        }
-
-        return $prompt;
-       
-  }
-
   
 
 }
