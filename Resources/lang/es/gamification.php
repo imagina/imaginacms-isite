d605e64a745cb4e7794e2ba04b9b77491c3fb7e9
Adriana <39056746+adrianacardoso@users.noreply.github.com>
2023-08-09 15:16:29 -0400

    +uppate: optimized code (#164)
    * +uppate: optimized code
    
    * +update: summary gamification

--- Resources/lang/es/gamification.php
+++ Resources/lang/es/gamification.php
@@ -30,5 +30,7 @@ return [
     "settingsOrganizationDescription" => "Administra las configuraciones de tu sitio",
     "adminOrganizationWizardWelcome" => "¡Bienvenido a Weygo!",
     "adminOrganizationWizardWelcomeDescription" => "Aquí podras crear tu sitio de manera sencilla y eficiente. te guiaremos en cada paso para que puedas expresar tu visión en línea. ¡Comencemos a crear tu sitio! 🚀",
+    "adminOrganizationWizardSummary" => "Resumen",
+    "adminOrganizationWizardSummaryDescription" => " ¡Felicidades! Has llegado al último paso del proceso de creación de tu sitio web. Ahora solo necesitas revisar los datos seleccionados y asegurarte de que todo esté en orden. ¡Gracias por elegirnos!",
   ],
 ];
