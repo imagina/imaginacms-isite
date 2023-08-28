449d37a00cf0b2056453755e119a0b3f4463478e
Juan Escalante <jcec007@gmail.com>
2023-08-28 10:11:58 -0500

    +Update: Added verification if $item is null to avoid error is pending check why the getData method is called twice

--- Http/Livewire/Index/ItemModal.php
+++ Http/Livewire/Index/ItemModal.php
@@ -50,7 +50,7 @@ class ItemModal extends Component
     $this->params = $params;
     $this->repository = $repository;
     $this->idModal = $idModal;
-     
+    
   }
 
   /*
@@ -61,22 +61,27 @@ class ItemModal extends Component
   {
 
     //\Log::info("ItemModal - GETDATA : {$itemId} - {$idModalNew}");
-
+  
     $item = $this->getItemRepository()->getItem($itemId,json_decode(json_encode($this->params)));
-
-     //'item' => json_decode(json_encode($item), FALSE)
-    $newHtml = view($this->view, [
-      'item' => $item,
-      'inModal' => true
-    ])->render();
-
-    if($idModalNew==$this->idModal){
-      $this->dispatchBrowserEvent('item-load-modal-content-'.$this->idModal, [
-        'newHtml' => $newHtml,
-        'itemUrl' => $item->url ?? '',
-        'idModalNew' => $idModalNew
-      ]);
+    //\Log::info("ItemModal - : ".($item->id ?? 'asdasd')." $this->repository"." ". json_encode($this->params));
+  
+    //TODO check why this method (getData) is called twice, in the meantime I'm doing this validation to avoid $item with null value in sometimes
+    if(isset($item->id)){
+      //'item' => json_decode(json_encode($item), FALSE)
+      $newHtml = view($this->view, [
+        'item' => $item,
+        'inModal' => true
+      ])->render();
+  
+      if($idModalNew==$this->idModal){
+        $this->dispatchBrowserEvent('item-load-modal-content-'.$this->idModal, [
+          'newHtml' => $newHtml,
+          'itemUrl' => $item->url ?? '',
+          'idModalNew' => $idModalNew
+        ]);
+      }
     }
+    
     
   }
 
