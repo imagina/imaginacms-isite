7cc7f9df5daf9a6bbfb972e53de57cb08a6a503f
Nicolas <nmontealegre@imaginacolombia.com>
2023-11-11 10:44:53 -0500

    Fixed: logic in sync service

--- Services/SynchronizableService.php
+++ Services/SynchronizableService.php
@@ -16,6 +16,8 @@ class SynchronizableService
   {
     $this->client = new \GuzzleHttp\Client();
     $this->n8nBaseUrl = setting('isite::n8nBaseUrl');
+    //Get url of app
+    $this->domain = env("APP_URL");
     $this->modelRepository = $modelRepository;
   }
 
@@ -23,45 +25,63 @@ class SynchronizableService
   {
     $syncConfig = config("asgard.{$params["module"]}.config.synchronizable");
 
-    // Get all sync by module
+    // Get name of the sync table
+    $nameSync = $params["module"] . '_sync' . ucfirst($params["entity"]);
+    // Get entity to create sheet
+    $entityToSync = $syncConfig["entities"][$nameSync] ?? [];
+    // Get template ID
+    $templateId =  $entityToSync["template_id"] ?? null;
+
+    if (!isset($templateId)) throw new Exception("Template not found", 404);
+    // Get a sync by name
     $currentModel = $this->modelRepository->getItemsBy(json_decode(json_encode([
-      'filter' => ["name" => ['where' => 'in', 'value' => $syncConfig['entities']]]
-    ])));
+      'filter' => ["name" => $nameSync]
+    ])))->first();
+
+    if (!isset($currentModel)) throw new Exception("Model {$nameSync} not found", 404);
     // Search for a sheetId
-    $sheetId = $currentModel->whereNotNull('sheet_id')->first();
+    $spreadsheetId = $currentModel->spreadsheet_id;
 
-    if (!$sheetId) {
-      // Create sheet and put the id on $sheetId
-      $oldEnabledEmails = explode(',', $currentModel->first()->enabled_emails) ?? null;
+    // Create sheet and put the id on $sheetId
+    $oldEnabledEmails = explode(',', $currentModel->first()->enabled_emails) ?? null;
 
-      $siteName = setting('core::site-name');
+    $siteName = setting('core::site-name');
 
-      $requestParams = [
-        "title" => "{$params["module"]} $siteName",
-        "template_sheet_id" => $syncConfig['template_id'],
-        "sheet_id" => null,
-        "old_enabled_emails" => $oldEnabledEmails,
-        "enabled_emails" => $params["enabled_emails"]
-      ];
+    $requestParams = [
+      "title" => "{$params["module"]} {$params["entity"]} $siteName",
+      "template_sheet_id" => $templateId,
+      "sheet_id" => $spreadsheetId,
+      "old_enabled_emails" => $oldEnabledEmails,
+      "enabled_emails" => $params["enabled_emails"]
+    ];
 
-      // Send a POST request to N8N to clone the spreadsheet
-      $response = $this->client->request('POST',
-        "$this->n8nBaseUrl/google-sheets/create/webhook",
-        [
-          "body" => json_encode($requestParams),
-          'headers' => [
-            'Content-Type' => 'application/json'
-          ]
+    if($spreadsheetId && is_null($currentModel->template_id)) $requestParams["action"] = 'delete';
+
+    // Send a POST request to N8N to clone the spreadsheet
+    $response = $this->client->request('POST',
+      "$this->n8nBaseUrl{$syncConfig["workflowPath"]}create/webhook",
+      [
+        "body" => json_encode($requestParams),
+        'headers' => [
+          'Content-Type' => 'application/json'
         ]
-      );
+      ]
+    );
 
-      $sheetId = json_decode($response->getBody()->getContents())->sheet_id;
-      // Sync the sheet_id attribute
-      foreach ($currentModel as $model) {
-        $model->update(['sheet_id' => $sheetId]);
+    $responseN8N = json_decode($response->getBody()->getContents());
+    $spreadsheetId = $responseN8N->sheet_id;
+    $sheets = $responseN8N->sheets;
+    $resultado = [];
+
+    // Save the sheets id for each sheet
+    foreach ($sheets as $objeto) {
+      if ($objeto->properties->title) {
+        $resultado = array_merge([$objeto->properties->title => $objeto->properties->sheetId], $resultado);
       }
-
     }
+    $emails = implode(',', $params["enabled_emails"]);
+    // Sync the sheet_id attribute
+    $currentModel->update(['spreadsheet_id' => $spreadsheetId,'template_id' => $templateId, 'sheets' => json_encode($resultado), 'enabled_emails' => $emails]);
 
 
     return ["data" => "Request successful"];
@@ -71,10 +91,12 @@ class SynchronizableService
   public function exportData($params)
   {
     $moduleName = explode('_', $params["name"])[0];
-    $urlToExport = "$this->n8nBaseUrl/google-sheets/export/";
+    $syncConfig = config("asgard.$moduleName.config.synchronizable");
+    $entityToSync = $syncConfig["entities"][$params["name"]] ?? null;
 
-    //Get url of app
-    $domain = env("APP_URL");
+    if(is_null($entityToSync)) throw new Exception("Entity to Export not found", 404);
+
+    $urlToExport = "$this->n8nBaseUrl{$syncConfig["workflowPath"]}export/";
 
     $currentModel = $this->modelRepository->getItem($params["name"], json_decode(json_encode([
       'filter' => ["field" => 'name']
@@ -84,6 +106,7 @@ class SynchronizableService
 
     if($currentModel->is_running) throw new Exception("process is runing", 405);
 
+    $sheets = json_decode($currentModel->sheets);
     // Retrieve the user ID who initiated the action
     $id = Auth::id();
     // Get token of soporte
@@ -91,12 +114,23 @@ class SynchronizableService
 
     $requestParams = [
       "name" => $params["name"],
-      "domain_url" => $domain,
-      "sheet_id" => $currentModel->sheet_id,
+      "domain_url" => $this->domain,
+      "spreadsheet_id" => $currentModel->spreadsheet_id,
+      "sheets" => $sheets,
       "access_token" => $token,
-      "requestParams" => $params["requestParams"]
+      "requestParams" => $params["requestParams"],
     ];
 
+    $functionality = 'export';
+
+    if(isset($params["exportDependencies"]) && $params["exportDependencies"]) {
+      $functionality = 'dependencies';
+      $requestParams["dependencies"] = $entityToSync["dependencies"];
+    } else {
+      $requestParams["api_route"] = $entityToSync["apiRoute"];
+      $requestParams["sheet_name"] = $entityToSync["sheetName"];
+      $requestParams["columns"] = $entityToSync["columns"] ?? null;
+    }
     // Send a POST request to N8N to export data to the spreadsheet
     $response = $this->client->request('POST',
       $urlToExport,
@@ -108,17 +142,20 @@ class SynchronizableService
       ]
     );
 
-    $updateModel = $currentModel->update(['is_running' => 'export', 'last_sync' => now(), 'exported_by_id' => $id]);
+    $updateModel = $currentModel->update(['is_running' => $functionality, 'last_sync' => now(), 'exported_by_id' => $id]);
 
     return ['data' => json_decode($response->getBody()->getContents())->message];
   }
 
   public function importData($params)
   {
-
-    if ($params["name"] === 'icommerce_syncCategories') throw new Exception("Cannot be imported", 400);
     $moduleName = explode('_', $params["name"])[0];
-    $urlToImport = "$this->n8nBaseUrl/google-sheets/import/$moduleName";
+    $syncConfig = config("asgard.$moduleName.config.synchronizable");
+    $entityToSync = $syncConfig["entities"][$params["name"]] ?? null;
+
+    if(is_null($entityToSync)) throw new Exception("Entity to Sync not found", 404);
+
+    $urlToImport = "$this->n8nBaseUrl{$syncConfig["workflowPath"]}import/";
 
     $currentModel = $this->modelRepository->getItem($params["name"], json_decode(json_encode([
       'filter' => ["field" => 'name']
@@ -128,19 +165,21 @@ class SynchronizableService
 
     if ($currentModel->is_running) throw new Exception("'{$params["name"]}' is runing", 400);
 
-    //Get url of app
-    $domain = env("APP_URL");
+    $sheets = json_decode($currentModel->sheets);
     // Retrieve the user ID who initiated the action
     $id = Auth::id();
     // Get token of soporte
     $token = $this->generateToken();
 
     $requestParams = [
-      "sheet_id" => $currentModel->sheet_id,
-      "domain_url" => $domain,
+      "spreadsheet_id" => $currentModel->spreadsheet_id,
+      "domain_url" => $this->domain,
+      "sheet_name" => $entityToSync["sheetName"],
       "name" => $params["name"],
+      "sheets" => $sheets,
+      "dependencies" => $entityToSync["dependencies"],
       "access_token" => $token,
-      "requestParams" => $params["requestParams"]
+      "requestParams" => $params["requestParams"],
     ];
 
     // Send a POST request to N8N to export data to the spreadsheet
