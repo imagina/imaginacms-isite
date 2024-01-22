301c741ef7a76c67fdacceaff4bdf3ddabe26e48
William Verde <williamverde10@gmail.com>
2024-01-22 12:22:00 -0400

    added: param icon to marker

--- Resources/views/frontend/components/map/google.blade.php
+++ Resources/views/frontend/components/map/google.blade.php
@@ -4,6 +4,12 @@
     let map;
     let bounds;
     let markers = [];
+    let urlMarkerIcon;
+
+    @if(isset($imageIcon) && !is_null($imageIcon))
+      urlMarkerIcon = "{{$imageIcon}}"
+    @endif
+
     
     //Validation Modal
     @if(!isset($inModal) || !$inModal)
@@ -47,19 +53,35 @@
     @endif
 
     /*
-    * Set Marker in Google Map
+    * Set Markers in Google Map (Case Item Maps | Livewire Component)
     */
     function setMarkerGoogle(bounds)
     {
       
       const itemPosition = {lat: parseFloat(locLat), lng: parseFloat(locLng)};
-
+      //const iconMarker = "https://dev-lip.ozonohosting.com/assets/media/marker-2.png?u=1705935415";
+    
       //Init Marker
       const googleMarker = new google.maps.Marker({
             position: itemPosition,
             map: map,
             title: locTitle,
+            label: locId.toString(),
+            icon: urlMarkerIcon
       });
+      
+      //Prueba de marcador personalizado | no funcionó, genera error en "marker"
+      /*
+      const priceTag = document.createElement("div");
+      priceTag.className = "price-tag";
+      priceTag.textContent = "$2.5M";
+      const googleMarker = new google.maps.marker.AdvancedMarkerView({
+            position: itemPosition,
+            map: map,
+            content: priceTag
+      });
+      */
+      //======================
 
       //Content POPUP
       var popup = new google.maps.InfoWindow({
