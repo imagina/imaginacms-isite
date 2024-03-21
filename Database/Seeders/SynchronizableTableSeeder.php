28fc38606f8c1b15e0b0edef7350d74ddde497db
NicolasMB <nmontealegre@imaginacolombia.com>
2024-03-21 12:34:28 -0500

    Added: new logic to sync

--- Database/Seeders/SynchronizableTableSeeder.php
+++ Database/Seeders/SynchronizableTableSeeder.php
@@ -23,13 +23,19 @@ class SynchronizableTableSeeder extends Seeder
         $configData = config('asgard.' . $lowercaseModule . '.config.synchronizable');
 
         if(isset($configData) && $configData) {
-          foreach ($configData["entities"] as $entity) {
+          foreach ($configData["entities"] as $entity => $values) {
             $syncData = Synchronizable::where('name', $entity)->first();
 
             if(!isset($syncData->id)) {
 
               Synchronizable::create([
-                'name' => $entity
+                'name' => $entity,
+                'template_id' => $values["template_id"]
+              ]);
+            } else if($syncData->template_id !== $values["template_id"]) {
+
+              $syncData->update([
+                'template_id' => null
               ]);
             }
           }
