622a958b81e028bf51cd0c8d2d3ece00370894e0
Adriana <39056746+adrianacardoso@users.noreply.github.com>
2024-04-29 16:09:02 -0400

    +update: block Multilang (#242)

--- Resources/views/frontend/components/multilang/layouts/multilang-layout-2/index.blade.php
+++ Resources/views/frontend/components/multilang/layouts/multilang-layout-2/index.blade.php
@@ -1,11 +1,17 @@
-<div id="multilanglayout2" class="multilang">
-  {{ LaravelLocalization::getCurrentLocale()}}
-
+<div id="multilanglayout2" class="multilang {{$multiflagClasses}}">
   <form>
-    <select id="inputState" class="">
+    <label for="inputState">
+        <span class="{{$classSelectOut}}">{{ LaravelLocalization::getCurrentLocale()}}</span>
+        @include('isite::frontend.components.multilang.partials.image', array(
+          'styleFlag' => !empty($flagStyles) ? $flagStyles : 'width: 35px; height: 33px; object-fit: cover;',
+          'classFlag' => !empty($flagClasses) ? $flagClasses : 'rounded-circle mx-2',
+          'locale' => LaravelLocalization::getCurrentLocale())
+        )
+    </label>
+    <select id="inputState" class="{{$classSelect}}">
       @foreach($locales as $locale)
         <option href="/{{$locale}}"
-                {{ LaravelLocalization::getCurrentLocale() == $locale ? "selected" : "" }} value="{{$locale}}">
+          {{ LaravelLocalization::getCurrentLocale() == $locale ? "selected" : "" }} value="{{$locale}}">
           {{$locale}}
         </option>
       @endforeach
