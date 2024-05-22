2853d840aaa9a9bd454ad52838946f2b3366087f
Cristian Marin <marin101998@gmail.com>
2024-05-22 17:35:00 -0500

    +fix | v10.x 22-05-24 change name of parameter internal

--- Traits/WithProduct.php
+++ Traits/WithProduct.php
@@ -10,53 +10,53 @@ use Modules\Icommerce\Entities\Product;
  */
 trait WithProduct
 {
-    /**
-     * Boot trait method
-     */
-    public static function bootWithProduct()
-    {
-        // Validate Only Icommerce Enabled
-        if (is_module_enabled('Icommerce')) {
-            //Listen event after create model
-            static::createdWithBindings(function ($model) {
-                $model->syncProduct();
-            });
+  /**
+   * Boot trait method
+   */
+  public static function bootWithProduct()
+  {
+    // Validate Only Icommerce Enabled
+    if (is_module_enabled('Icommerce')) {
+      //Listen event after create model
+      static::createdWithBindings(function ($model) {
+        $model->syncProduct();
+      });
 
-            static::updatedWithBindings(function ($model) {
-                $model->syncProduct();
-            });
-        }
+      static::updatedWithBindings(function ($model) {
+        $model->syncProduct();
+      });
     }
+  }
 
-    /**
-     * Sync Product
-     */
-    public function syncProduct()
-    {
-        $product = \Modules\Icommerce\Entities\Product::updateOrCreate([
-            'entity_type' => get_class($this),
-            'entity_id' => $this->id,
-        ], [
-            'name' => $this->title ?? $this->name,
-            'slug' => $this->slug ?? istr_slug($this->title ?? $this->name),
-            'description' => $this->description ?? '',
-            'summary' => $this->summary ?? substr($this->description, 0, 150) ?? '',
-            'price' => $this->price ?? 0,
-            'status' => $this->status ?? 1, //ENABLED
-            'stock_status' => $this->stock_status ?? 1, //INSTOCK
-            'quantity' => $this->quantity ?? 999999,
-            'shipping' => $this->requiredShipping ?? 1,
-            'internal' => 1,
-        ]);
-    }
+  /**
+   * Sync Product
+   */
+  public function syncProduct()
+  {
+    $product = \Modules\Icommerce\Entities\Product::updateOrCreate([
+      'entity_type' => get_class($this),
+      'entity_id' => $this->id,
+    ], [
+      'name' => $this->title ?? $this->name,
+      'slug' => $this->slug ?? istr_slug($this->title ?? $this->name),
+      'description' => $this->description ?? '',
+      'summary' => $this->summary ?? substr($this->description, 0, 150) ?? '',
+      'price' => $this->price ?? 0,
+      'status' => $this->status ?? 1, //ENABLED
+      'stock_status' => $this->stock_status ?? 1, //INSTOCK
+      'quantity' => $this->quantity ?? 999999,
+      'shipping' => $this->requiredShipping ?? 1,
+      'is_internal' => 1,
+    ]);
+  }
 
-    /**
-     * Make the Productable morph relation
-     */
-    public function products(): object
-    {
-        return $this->morphMany("Modules\Icommerce\Entities\Product", 'entity');
-    }
+  /**
+   * Make the Productable morph relation
+   */
+  public function products(): object
+  {
+    return $this->morphMany("Modules\Icommerce\Entities\Product", 'entity');
+  }
 
   public function getProductAttribute()
   {
