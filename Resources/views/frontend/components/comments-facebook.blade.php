4338220778d7f467e4f9b08adb27311894672019
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2024-07-26 12:19:52 -0500

    +Add : Front-End | Component Comments Facebook

---
+++ Resources/views/frontend/components/comments-facebook.blade.php
@@ -0,0 +1,12 @@
+<div id="fb-root"></div>
+<div class="fb-comments w-100 {{ $classNames }}"
+     data-href="{{ $url }}"
+     data-numposts="{{$numComments}}"
+     data-width="100%">
+
+</div>
+<script
+  async defer crossorigin="anonymous"
+  src="{{$urlSrc}}"
+  nonce="{{$nonce}}">
+</script>
