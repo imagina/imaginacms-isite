b83212d45493767fb39a83df794346bc28029b03
msolanogithub <msolano@imaginacolombia.com>
2024-07-30 18:09:30 -0500

    implement reCaptcha v3 for blade

--- Resources/views/frontend/components/captcha.blade.php
+++ Resources/views/frontend/components/captcha.blade.php
@@ -1,24 +1,53 @@
 @if($captchaEnabled && $captchaEnabled == '1')
+  <!--Template to reCaptcha v2-->
+  @if($captchaVersion == '2')
     {!! app('icaptcha')->display($params) !!}
-    @once
-        @section('scripts-owl')
-             @parent
-            <script type="text/javascript" src="https://www.google.com/recaptcha/api.js" async></script>
-        @endsection
-    @endonce
+  @endif
+
+  <!-- Added reCaptcha CDN-->
+  @once
     @section('scripts-owl')
-        @parent
-        <script type="text/javascript">
-          $(function(){
-            disable{{ $formId }}Button();
-          });
-          function enable{{ $formId }}Button(response){
-            if(response)
-              $("#{{ $formId }} input[type=submit], #{{ $formId }} button[type=submit]").removeAttr('disabled');
-          }
-          function disable{{ $formId }}Button(){
-            $("#{{ $formId }} input[type=submit], #{{ $formId }} button[type=submit]").attr('disabled','disabled');
-          }
-        </script>
-    @stop
+      @parent
+      <script type="text/javascript" src="{{$jsApiUrl}}" async></script>
+    @endsection
+  @endonce
+
+  <!-- Logic to handle the token -->
+  @section('scripts-owl')
+    @parent
+    <script type="text/javascript">
+      //Get the submit element
+      const submitElement = $("#{{ $formId }} input[type=submit], #{{ $formId }} button[type=submit]");
+
+      $(function () {
+        //Disable the submit  button by default if it is the v2
+        if ({{$captchaVersion}} == '2') disable{{ $formId }}Button();
+        //Set the needed attributes in submit element to use v3
+        else {
+          submitElement.addClass("g-recaptcha");
+          submitElement.attr('data-sitekey', "{{$captchaKey}}")
+          submitElement.attr('data-action', "submit")
+          submitElement.attr('data-callback', "onSubmit{{$formId}}Form");
+        }
+      });
+
+      //Enable form button submit
+      function enable{{ $formId }}Button(response) {
+        if (response) submitElement.removeAttr('disabled');
+      }
+
+      //Disable
+      function disable{{ $formId }}Button() {
+        submitElement.attr('disabled', 'disabled');
+      }
+
+      //Handle onSubmit when v3
+      function onSubmit{{$formId}}Form() {
+        const form = $("#{{ $formId }}");
+        //Validate form before submit
+        if (form.get(0).checkValidity()) form.submit()
+        else form.get(0).reportValidity()
+      }
+    </script>
+  @stop
 @endif
--- View/Components/Captcha.php
+++ View/Components/Captcha.php
@@ -8,6 +8,9 @@ class Captcha extends Component
 {
   public $formId;
   public $captchaEnabled;
+  public $captchaVersion;
+  public $captchaKey;
+  public $jsApiUrl;
   public $params;
 
   /**
@@ -15,13 +18,27 @@ class Captcha extends Component
    *
    * @return void
    */
-  public function __construct($formId = 'formsuscripcion', $params = [])
+  public function __construct($formId = "formsuscripcion", $params = [])
   {
-    $this->formId = $formId ?? 'formsuscripcion';
+    //Get keys
+    $keysv2 = setting('isite::reCaptchaV2Site');
+    $keysv3 = setting('isite::reCaptchaV3Site');
+
+    //Instance the captcha key to use
+    $this->captchaKey = $keysv3 ?? $keysv2;
+
+    //Define the captcha version to use, with priority in the v3
+    $this->captchaVersion = $keysv3 ? '3' : ($keysv2 ? '2' : null);
+
+    //Intance the reCaptcha js api URL
+    $this->jsApiUrl = "https://www.google.com/recaptcha/api.js";
+    if ($this->captchaVersion == '3') $this->jsApiUrl .= "?render=$keysv3";
+
+    $this->formId = $formId ?? "formsuscripcion";
     $this->captchaEnabled = setting('isite::activateCaptcha', null, '0');
     $this->params = array_merge($params ?? [], [
       'id' => 'captcha' . $this->formId,
-      'data-sitekey' => setting('isite::reCaptchaV2Site') ?? setting('isite::reCaptchaV3Site'),
+      'data-sitekey' => $this->captchaKey,
       'data-callback' => "enable{$formId}Button",
       'data-expired-callback' => "disable{$formId}Button",
       'data-error-callback' => "disable{$formId}Button",
@@ -35,6 +52,6 @@ class Captcha extends Component
    */
   public function render()
   {
-    return view('isite::frontend.components.captcha');
+    return view("isite::frontend.components.captcha");
   }
 }
