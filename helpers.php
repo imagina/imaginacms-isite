e98dd37ca764f3a3f78f4427940916185285977d
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2024-07-31 11:21:32 -0500

    +Fix : Front-End | Validate function user Agent 31-07-2024

--- helpers.php
+++ helpers.php
@@ -153,9 +153,13 @@ if (!function_exists('generatePassword')) {
 if (!function_exists('isMobileDevice')) {
   function isMobileDevice()
   {
-    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo
-|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i"
-      , $_SERVER["HTTP_USER_AGENT"]);
+    if(isset($_SERVER["HTTP_USER_AGENT"])){
+      return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo
+              |fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i"
+        , $_SERVER["HTTP_USER_AGENT"]);
+    }else{
+      return false;
+    }
   }
 }
 
