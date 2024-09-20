82ac35d09712a50a8da4e2312c187b5de28db064
michael <msolano2505@gmail.com>
2024-09-19 21:58:53 -0500

    moved the helper convertMinutesToHumanReadable

--- helpers.php
+++ helpers.php
@@ -457,3 +457,38 @@ if (!function_exists('humanizeDuration')) {
       ($minutes > 0 ? $minutes . ' ' . trans('isite::isite.minutes') : '');
   }
 }
+
+if (! function_exists('convertMinutesToHumanReadable')) {
+  function convertMinutesToHumanReadable($minutes)
+  {
+    if (!$minutes) return 0;
+    if ($minutes < 60) {
+      return $minutes . ' ' . trans('itask::tasks.minutes');
+    }
+
+    $weeks = floor($minutes / (60 * 24 * 7));
+    $days = floor(($minutes % (60 * 24 * 7)) / (60 * 24));
+    $hours = floor(($minutes % (60 * 24)) / 60);
+    $remainingMinutes = $minutes % 60;
+
+    $result = '';
+
+    if ($weeks > 0) {
+      $result .= $weeks . ' ' . trans('itask::tasks.weeks') . ' ';
+    }
+
+    if ($days > 0) {
+      $result .= $days . ' ' . trans('itask::tasks.days') . ' ';
+    }
+
+    if ($hours > 0) {
+      $result .= $hours . ' ' . trans('itask::tasks.hours') . ' ';
+    }
+
+    if ($remainingMinutes > 0) {
+      $result .= trans('itask::tasks.and') . ' ' . $remainingMinutes . ' ' . trans('itask::tasks.minutes');
+    }
+
+    return $result;
+  }
+}
