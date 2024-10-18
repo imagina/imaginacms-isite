d1b04e20113ada715c0802910f4db65d51771cb5
Cristian Marin <marin101998@gmail.com>
2024-10-18 17:28:56 -0500

    + fix | v8 add auditstamps in recomendations table

---
+++ Database/Migrations/2024_10_18_171331_add_audistamp_in_recommendations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAudistampInRecommendationsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('isite__recommendations', function (Blueprint $table) {
+      $table->auditStamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    //
+  }
+}
