0f88d406195628d950276103dfd84d34a9b2d302
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2025-03-09 19:37:01 -0500

    Fixed: Added $id in carousel functions to ensure proper invocation.

--- Resources/views/frontend/components/owl/carousel.blade.php
+++ Resources/views/frontend/components/owl/carousel.blade.php
@@ -229,7 +229,7 @@
       });
     }
 
-    function refreshOwl(){
+    function refreshOwl{{$id}}(){
 
       createOWL{{$id}}();
 
@@ -244,19 +244,19 @@
     }
 
     // Function to check if DOM is ready and initialize OWL
-    function initOWL() {
+    function initOWL{{$id}}() {
       createOWL{{$id}}();
 
       @if($nav && $navPosition=="center")
-      window.addEventListener('owlRefreshed', refreshOwl())
-      refreshOwl();
+      window.addEventListener('owlRefreshed', refreshOwl{{$id}}())
+      refreshOwl{{$id}}();
       @endif
     }
 
     if (document.readyState != 'complete') {
-      document.addEventListener('DOMContentLoaded', initOWL);
+      document.addEventListener('DOMContentLoaded', initOWL{{$id}});
     } else {
-      initOWL(); // DOM is already loaded, initialize immediately
+      initOWL{{$id}}(); // DOM is already loaded, initialize immediately
     }
   </script>
 
