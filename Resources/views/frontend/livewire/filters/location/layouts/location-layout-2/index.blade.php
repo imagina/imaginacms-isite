b52b7aac3636146a8f0f8f9b1fb5598c6c00a009
Cristian Marin <54483189+CristianMarin19@users.noreply.github.com>
2025-04-30 15:04:57 -0500

    + fix | v10 29-04-25 bugs params in function and params reload page (#336)

--- Resources/views/frontend/livewire/filters/location/layouts/location-layout-2/index.blade.php
+++ Resources/views/frontend/livewire/filters/location/layouts/location-layout-2/index.blade.php
@@ -1,96 +1,104 @@
 <div class="filter-{{$type}} filter-{{$type}}-layout-{{$layout}} filter-{{$name}}">
 
-    @if($options && count($options)>0)
-        <div class="dropdown radio-location">
-            <button class="btn btn-selected" type="button" id="dropdownMenuLocation2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="selected-radio">{{$selectedOptionName}}</span>
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuLocation2">
-                <div class="options-container">
-                @foreach($options as $key => $option)
-                    <div class="option-radio-location">
-                        <input class="radio" type="radio"
-                            value="{{$option->id}}"
-                            name="radioOp"
-                            id="radioOp{{$key}}"
-                            wire:model="selectedOption">
-                        <label class="form-check-label"
-                            for="radioOp{{$key}}">
-                            {{strtolower($option->name)}}
-                        </label>
-                    </div>
-                @endforeach
-                </div>
+  @if($options && count($options)>0)
+    <div class="dropdown radio-location">
+      <button class="btn btn-selected" type="button" id="dropdownMenuLocation2" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+        <span class="selected-radio">{{$selectedOptionName}}</span>
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuLocation2">
+        <div class="options-container">
+          @foreach($options as $key => $option)
+            <div class="option-radio-location">
+              <input class="radio" type="radio"
+                     value="{{$option->id}}"
+                     name="radioOp"
+                     id="radioOp{{$key}}"
+                     wire:model="selectedOption">
+              <label class="form-check-label"
+                     for="radioOp{{$key}}">
+                {{strtolower($option->name)}}
+              </label>
             </div>
+          @endforeach
         </div>
-    @endif
-    
+      </div>
+    </div>
+  @endif
+
 </div>
 
 @section('scripts-owl')
-    @parent
-    <script>
-        // emit To Parent Filter
-        function emitToParentFilterLocation(selectedOptionId,updateItemList){
+  @parent
+  <script>
 
-            //console.warn("FILTER LOCATION - FRONTEND - SELECTED OPTION: "+selectedOptionId)
-            setTimeout(function(){
+    window.addEventListener('reload-page', () => {
+      window.location.reload();
+    });
 
-                window.livewire.emit('filtersGetData',{
-                    'name' : 'location-range',
-                    'filter' : {
-                        'cityId':selectedOptionId  
-                    },
-                    'eventUpdateItemsList':updateItemList
-                }) 
+    // emit To Parent Filter
+    function emitToParentFilterLocation(selectedOptionId, updateItemList) {
 
-            }, 3000);
-           
-        }
-       
-        /*
-        * Call Back - Response Api
-        */
-        function callback(data)
-        {
+      //console.warn("FILTER LOCATION - FRONTEND - SELECTED OPTION: "+selectedOptionId)
+      setTimeout(function () {
 
-            //console.warn(data)
-            @this.city = data.city;
-            @this.province = data.state;
-            @this.country = data.country_name;
-            @this.countryCode = data.country_code;
-           
-        }
+        window.livewire.emit('filtersGetData', {
+          'name': 'location-range',
+          'filter': {
+            'cityId': selectedOptionId
+          },
+          'eventUpdateItemsList': updateItemList
+        })
 
-        /*
-        * Init Script Geolocation
-        */
-        function initGeolocation(){
+      }, 3000);
 
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://geolocation-db.com/jsonp';
-            var h = document.getElementsByTagName('script')[0];
-            h.parentNode.insertBefore(script, h);
-        }
+    }
 
-        /**
-        * Start Geolocation
-        */
-        @php ($cityIdSelectedSession = request()->session()->get('cityIdSelected'));
+    /*
+    * Call Back - Response Api
+    */
+    function callback(data) {
 
-        // Not Exist Session
-        // Value 0 = All / Todos
-        @if(!$cityIdSelectedSession)
-            initGeolocation()
-        @else
-            @if($cityIdSelectedSession!=0)
-                emitToParentFilterLocation({{$cityIdSelectedSession}},true)
-            @endif
-        @endif
-       
-       
+      //console.warn(data)
+    @this.city
+      = data.city;
+    @this.province
+      = data.state;
+    @this.country
+      = data.country_name;
+    @this.countryCode
+      = data.country_code;
 
-    </script>
+    }
+
+    /*
+    * Init Script Geolocation
+    */
+    function initGeolocation() {
+
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://geolocation-db.com/jsonp';
+      var h = document.getElementsByTagName('script')[0];
+      h.parentNode.insertBefore(script, h);
+    }
+
+    /**
+     * Start Geolocation
+     */
+    @php ($cityIdSelectedSession = request()->session()->get('cityIdSelected'));
+
+    // Not Exist Session
+    // Value 0 = All / Todos
+    @if(!$cityIdSelectedSession)
+    initGeolocation()
+    @else
+    @if($cityIdSelectedSession!=0)
+    emitToParentFilterLocation({{$cityIdSelectedSession}}, true)
+    @endif
+    @endif
+
+
+  </script>
 
 @stop
