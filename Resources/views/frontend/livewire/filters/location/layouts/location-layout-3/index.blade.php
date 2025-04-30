f07aa49f271a6ae017c0934871c0ac225d32da37
Cristian Marin <marin101998@gmail.com>
2025-04-30 14:56:59 -0500

    + fix | v10 29-04-25 bugs params in function and params reload page

--- Resources/views/frontend/livewire/filters/location/layouts/location-layout-3/index.blade.php
+++ Resources/views/frontend/livewire/filters/location/layouts/location-layout-3/index.blade.php
@@ -1,88 +1,94 @@
 <div class="filter-{{$type}} filter-{{$type}}-layout-{{$layout}} filter-{{$name}}">
 
-    @if($options && count($options)>0)
+  @if($options && count($options)>0)
 
-        <select class="form-control form-control-sm"
-            wire:model="selectedOption" 
+    <select class="form-control form-control-sm"
+            wire:model="selectedOption"
             wire:ignore>
-            <option value="0">{{trans('isite::frontend.filter-location.all')}}</option>
-            @foreach($options as $key => $option)
-                <option value="{{$option->id}}">
-                   {{$option->name}}
-                </option>
-            @endforeach
-        </select>
+      <option value="0">{{trans('isite::frontend.filter-location.all')}}</option>
+      @foreach($options as $key => $option)
+        <option value="{{$option->id}}">
+          {{$option->name}}
+        </option>
+      @endforeach
+    </select>
 
-    @endif
-   
+  @endif
+
 </div>
 
 @section('scripts-owl')
-    @parent
+  @parent
 
-    <script>
+  <script>
 
-        // emit To Parent Filter
-        function emitToParentFilterLocation(selectedOptionId,updateItemList){
+    window.addEventListener('reload-page', () => {
+      window.location.reload();
+    });
 
-            //console.warn("FILTER LOCATION - FRONTEND - SELECTED OPTION: "+selectedOptionId)
-            setTimeout(function(){
+    // emit To Parent Filter
+    function emitToParentFilterLocation(selectedOptionId, updateItemList) {
 
-                window.livewire.emit('filtersGetData',{
-                    'name' : 'location-range',
-                    'filter' : {
-                        'cityId':selectedOptionId  
-                    },
-                    'eventUpdateItemsList':updateItemList
-                }) 
+      //console.warn("FILTER LOCATION - FRONTEND - SELECTED OPTION: "+selectedOptionId)
+      setTimeout(function () {
 
-            }, 3000);
-           
-        }
-       
-        /*
-        * Call Back - Response Api
-        */
-        function callback(data)
-        {
+        window.livewire.emit('filtersGetData', {
+          'name': 'location-range',
+          'filter': {
+            'cityId': selectedOptionId
+          },
+          'eventUpdateItemsList': updateItemList
+        })
 
-            //console.warn(data)
-            @this.city = data.city;
-            @this.province = data.state;
-            @this.country = data.country_name;
-            @this.countryCode = data.country_code;
-           
-        }
+      }, 3000);
 
-        /*
-        * Init Script Geolocation
-        */
-        function initGeolocation(){
+    }
 
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://geolocation-db.com/jsonp';
-            var h = document.getElementsByTagName('script')[0];
-            h.parentNode.insertBefore(script, h);
-        }
+    /*
+    * Call Back - Response Api
+    */
+    function callback(data) {
 
-        /**
-        * Start Geolocation
-        */
-        @php ($cityIdSelectedSession = request()->session()->get('cityIdSelected'));
+      //console.warn(data)
+    @this.city
+      = data.city;
+    @this.province
+      = data.state;
+    @this.country
+      = data.country_name;
+    @this.countryCode
+      = data.country_code;
 
-        // Not Exist Session
-        // Value 0 = All / Todos
-        @if(!$cityIdSelectedSession)
-            initGeolocation()
-        @else
-            @if($cityIdSelectedSession!=0)
-                emitToParentFilterLocation({{$cityIdSelectedSession}},true)
-            @endif
-        @endif
-       
-       
+    }
 
-    </script>
+    /*
+    * Init Script Geolocation
+    */
+    function initGeolocation() {
+
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://geolocation-db.com/jsonp';
+      var h = document.getElementsByTagName('script')[0];
+      h.parentNode.insertBefore(script, h);
+    }
+
+    /**
+     * Start Geolocation
+     */
+    @php ($cityIdSelectedSession = request()->session()->get('cityIdSelected'));
+
+    // Not Exist Session
+    // Value 0 = All / Todos
+    @if(!$cityIdSelectedSession)
+    initGeolocation()
+    @else
+    @if($cityIdSelectedSession!=0)
+    emitToParentFilterLocation({{$cityIdSelectedSession}}, true)
+    @endif
+    @endif
+
+
+  </script>
 
 @stop
