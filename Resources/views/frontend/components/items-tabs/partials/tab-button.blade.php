15a0893d4c13b9aedaff7bf55b793a68946dc9bb
josuebedoya348 <145929750+josuebedoya@users.noreply.github.com>
2025-07-14 15:09:24 -0500

    Fix button URL from tabsList (#354)

--- Resources/views/frontend/components/items-tabs/partials/tab-button.blade.php
+++ Resources/views/frontend/components/items-tabs/partials/tab-button.blade.php
@@ -1,7 +1,7 @@
 @if($withTabBtn)
   <div class="tab-pane-btn {{$tabBtnAlign ?? ''}}">
     <a role="button" aria-label="{{trans('isite::common.button.tabSeeAll')}}" class="button-base {{$tabBtnClass ?? ''}} button-tabs"
-       href="/tienda/c/{{$item->slug}}" target="_self">
+       href="{{$item->url}}" target="_self">
       <span> {{trans("isite::common.button.tabSeeAll")}} </span>
     </a>
   </div>
