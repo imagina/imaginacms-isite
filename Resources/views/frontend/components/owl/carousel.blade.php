b7f1e9085b3c0536bae45c06c5706dc8e509dfc3
msolanogithub <msolano2505@gmail.com>
2025-08-04 16:25:13 -0500

    Refactor carousel component for improved item handling and performance

--- Resources/views/frontend/components/owl/carousel.blade.php
+++ Resources/views/frontend/components/owl/carousel.blade.php
@@ -8,14 +8,14 @@
           <div class="title-section {{$owlTextAlign}} @if($owlTextPosition==3) d-flex flex-column @endif ">
             @if($title!=="")
               @if($owlTitleUrl)
-              <a href="{{$owlTitleUrl}}" target="{{$owlTitleTarget}}" class="text-decoration-none">
-              @endif
-              <h2 class="title {{$owlTitleClasses}} {{ $owlTextPosition==3 ? 'order-1':'' }} {{$owlTitleColor}} {{$owlTitleWeight}} {{$owlTitleTransform}} {{$owlTitleMarginT}} {{$owlTitleMarginB}}">
-                @if($owlTitleVineta) <i class="{{$owlTitleVineta}} {{$owlTitleVinetaColor}} mr-1"></i>  @endif
-                <span> {!! $title !!}</span>
-              </h2>
-              @if($owlTitleUrl)
-              </a>
+                <a href="{{$owlTitleUrl}}" target="{{$owlTitleTarget}}" class="text-decoration-none">
+                  @endif
+                  <h2 class="title {{$owlTitleClasses}} {{ $owlTextPosition==3 ? 'order-1':'' }} {{$owlTitleColor}} {{$owlTitleWeight}} {{$owlTitleTransform}} {{$owlTitleMarginT}} {{$owlTitleMarginB}}">
+                    @if($owlTitleVineta) <i class="{{$owlTitleVineta}} {{$owlTitleVinetaColor}} mr-1"></i>  @endif
+                    <span> {!! $title !!}</span>
+                  </h2>
+                  @if($owlTitleUrl)
+                </a>
               @endif
             @endif
             @if($subTitle!=="" && $owlTextPosition!=1)
@@ -98,49 +98,25 @@
           </div>
 
           @endif
-          @if(!empty($itemComponentAttributes["itemDelay"]))
-            @php($delay = $itemComponentAttributes['itemDelay'])
-            @php($cont=0)
-          @endif
           <div class="@if($navPosition!="center" || ($nav==false && $navPosition=="center")) row py-3 @endif">
             <div id="{{$id}}Carousel" class="owl-carousel owl-theme {{$dotsStyle}}">
-              @php($x = 0) {{-- iterador de items --}}
-              @php($j = 0) {{-- iterador de itemsBySlide --}}
-              @while(isset($items[$x]))
-                @php($j = 0)
-                @if($itemsBySlide > 1)
-
-                  <div class="items-by-slide">
+              @foreach($chunkedItems as $chunkIndex => $chunk)
+                <div class="items-by-slide">
+                  @foreach($chunk as $itemIndex => $itemData)
+                    @if($typeComponent && !empty($itemData['item']->code_ads))
+                      <div class="banner-{{$itemData['item']->id}}">
+                        {!! $itemData['item']->code_ads !!}
+                      </div>
+                    @else
+                      @include("isite::frontend.partials.item",[
+                        "item" => $itemData['item'],
+                        "itemComponentAttributes" => $itemData['componentAttributes'],
+                        "position" => ($chunkIndex * $itemsBySlide) + $itemIndex
+                      ])
                     @endif
-
-                    @while(isset($items[$x + $j]) && $j<$itemsBySlide)
-
-                      @if(!empty($itemComponentAttributes["itemDelay"]))
-                          @php($itemComponentAttributes["itemDelay"]=$delay+$cont)
-                          @if(!empty($itemComponentAttributes["itemDelayIn"]))
-                              @php($cont=$cont+intval($itemComponentAttributes['itemDelayIn']))
-                          @endif
-                      @endif
-                      @if($typeComponent && !empty($items[$x + $j]->code_ads))
-                        <div class="banner-{{$items[$x + $j]->id}}">
-                          {!! $items[$x + $j]->code_ads !!}
-                        </div>
-                      @else
-                        @if($typeComponent)
-                          @php($itemComponentAttributes["viewMoreButtonLabel"]=$items[$x + $j]->caption)
-                          @php($itemComponentAttributes["target"]=$items[$x + $j]->target)
-                        @endif
-
-                        @include("isite::frontend.partials.item",["item" => $items[$x + $j], "position" => $x + $j])
-                      @endif
-
-                      @php($j++)
-                    @endwhile
-                    @if($itemsBySlide > 1)
-                  </div>
-                @endif
-                @php($x+=$itemsBySlide)
-              @endwhile
+                  @endforeach
+                </div>
+              @endforeach
             </div>
           </div>
 
@@ -231,23 +207,23 @@
 
       function refreshOwl(){
 
-          createOWL{{$id}}();
+        createOWL{{$id}}();
 
-          let sizeButton = document.querySelector('[id="{{$id}}"] .prevBtn');
-          let width = (sizeButton.offsetWidth) + 2;
-          let wrapper = document.querySelector('[id="{{$id}}"] .wrapper');
-          let w = (width)*2 + 9;
-          if(wrapper != null) {
-            wrapper.style.cssText = 'grid-template-columns: '+width+'px calc(100% - '+w+'px) '+width+'px';
-          }
+        let sizeButton = document.querySelector('[id="{{$id}}"] .prevBtn');
+        let width = (sizeButton.offsetWidth) + 2;
+        let wrapper = document.querySelector('[id="{{$id}}"] .wrapper');
+        let w = (width)*2 + 9;
+        if(wrapper != null) {
+          wrapper.style.cssText = 'grid-template-columns: '+width+'px calc(100% - '+w+'px) '+width+'px';
+        }
 
       }
 
-     createOWL{{$id}}();
+      createOWL{{$id}}();
 
       @if($nav && $navPosition=="center")
-        window.addEventListener('owlRefreshed', refreshOwl())
-        refreshOwl();
+      window.addEventListener('owlRefreshed', refreshOwl())
+      refreshOwl();
       @endif
     });
 
@@ -265,99 +241,99 @@
       margin-left: -15px;
     }
     @media (max-width: 768px) {
-        #{{$id}} .wrapper {
-            grid-template-columns: 1fr 1fr !important;
-        }
-        #{{$id}} .wrapper > div:nth-child(1) {
-             text-align: right;
-         }
-        #{{$id}} .wrapper > div:nth-child(2) {
-             grid-row: 1/1;
-             grid-column: 1/3;
-        }
+      #{{$id}} .wrapper {
+      grid-template-columns: 1fr 1fr !important;
+    }
+      #{{$id}} .wrapper > div:nth-child(1) {
+      text-align: right;
+    }
+      #{{$id}} .wrapper > div:nth-child(2) {
+                            grid-row: 1/1;
+                            grid-column: 1/3;
+                          }
     }
     @endif
     @if($dots)
     #{{$id}} .owl-carousel .owl-dots button:focus {
-         outline: 0 !important;
-    }
+           outline: 0 !important;
+         }
     @if($dotsStyle=="dots-linear")
         @if(empty($dotsStyleColor)) @php($dotsStyleColor='primary') @endif
         @if(empty($dotsSize)) @php($dotsSize='25') @endif
         #{{$id}} .dots-linear .owl-dots .owl-dot span {
-                 width: {{$dotsSize}}px;
-                 height: 5px;
-                 margin: 0 5px;
-                 border-radius: 0;
-        }
-        #{{$id}} .dots-linear .owl-dots .owl-dot.active span,
-        #{{$id}} .dots-linear .owl-dots .owl-dot:hover span {
-         background: var(--{{$dotsStyleColor}});
-        }
+               width: {{$dotsSize}}px;
+               height: 5px;
+               margin: 0 5px;
+               border-radius: 0;
+             }
+    #{{$id}} .dots-linear .owl-dots .owl-dot.active span,
+    #{{$id}} .dots-linear .owl-dots .owl-dot:hover span {
+       background: var(--{{$dotsStyleColor}});
+     }
     @endif
     @if($dotsStyle=="dots-circular")
         @if(empty($dotsSize)) @php($dotsSize='10') @endif
         #{{$id}} .dots-circular .owl-dots .owl-dot span {
-             width: {{$dotsSize}}px;
-             height: {{$dotsSize}}px;
-        }
-        #{{$id}} .dots-circular .owl-dots .owl-dot.active span,
-        #{{$id}} .dots-circular .owl-dots .owl-dot:hover span {
-            background:  var(--{{$dotsStyleColor}});
-        }
+               width: {{$dotsSize}}px;
+               height: {{$dotsSize}}px;
+             }
+    #{{$id}} .dots-circular .owl-dots .owl-dot.active span,
+    #{{$id}} .dots-circular .owl-dots .owl-dot:hover span {
+       background:  var(--{{$dotsStyleColor}});
+     }
     @endif
     @if($dotsStyle=="dots-square")
         @if(empty($dotsSize)) @php($dotsSize='10') @endif
         #{{$id}} .dots-square .owl-dots .owl-dot span {
-             width: {{$dotsSize}}px;
-             height: {{$dotsSize}}px;
-             border-radius: 0;
-       }
-        #{{$id}} .dots-square .owl-dots .owl-dot.active span,
-        #{{$id}} .dots-square .owl-dots .owl-dot:hover span {
-            background:  var(--{{$dotsStyleColor}});
-        }
+               width: {{$dotsSize}}px;
+               height: {{$dotsSize}}px;
+               border-radius: 0;
+             }
+    #{{$id}} .dots-square .owl-dots .owl-dot.active span,
+    #{{$id}} .dots-square .owl-dots .owl-dot:hover span {
+       background:  var(--{{$dotsStyleColor}});
+     }
     @endif
     @if($dotsStyle=="dots-oval")
         @if(empty($dotsSize)) @php($dotsSize='13') @endif
         #{{$id}} .dots-oval .owl-dots .owl-dot span {
-             width: {{$dotsSize}}px;
-             height: 8px;
-             border-radius: 10px;
-        }
-        #{{$id}} .dots-oval .owl-dots .owl-dot.active span,
-        #{{$id}} .dots-oval .owl-dots .owl-dot:hover span {
-            background:  var(--{{$dotsStyleColor}});
-        }
+               width: {{$dotsSize}}px;
+               height: 8px;
+               border-radius: 10px;
+             }
+    #{{$id}} .dots-oval .owl-dots .owl-dot.active span,
+    #{{$id}} .dots-oval .owl-dots .owl-dot:hover span {
+       background:  var(--{{$dotsStyleColor}});
+     }
     @endif
     @if($dotsStyle=="dots-circular-double" || $dotsStyle=="dots-square-double")
         @if(empty($dotsStyleColor)) @php($dotsStyleColor='primary') @endif
         @if(empty($dotsSize)) @php($dotsSize='13') @endif
         #{{$id}} .{{$dotsStyle}} .owl-dots .owl-dot span {
-             width: {{$dotsSize}}px;
-             height: {{$dotsSize}}px;
-             margin: 5px;
-             border: 2px solid var(--{{$dotsStyleColor}});
-             background-color: var(--{{$dotsStyleColor}});
-             position: relative;
-             background-clip: border-box;
-             opacity: 1;
-             flex: 0 1 auto;
-             @if($dotsStyle=="dots-square-double") border-radius: 0; @endif
+               width: {{$dotsSize}}px;
+               height: {{$dotsSize}}px;
+               margin: 5px;
+               border: 2px solid var(--{{$dotsStyleColor}});
+               background-color: var(--{{$dotsStyleColor}});
+               position: relative;
+               background-clip: border-box;
+               opacity: 1;
+               flex: 0 1 auto;
+               @if($dotsStyle=="dots-square-double") border-radius: 0; @endif
         }
-        #{{$id}} .{{$dotsStyle}} .owl-dots .owl-dot.active span {
-            border: 2px solid var(--{{$dotsStyleColor}});
-            background-color: transparent;
-        }
-        #{{$id}} .{{$dotsStyle}} .owl-dots .owl-dot.active span:after{
-             width: {{$dotsSize - 8 }}px;
-             height: {{$dotsSize - 8 }}px;
-             background-color: var(--{{$dotsStyleColor}});
-             bottom: 2px !important;
-             left: 2px !important;
-             content: "";
-             position: absolute;
-             @if($dotsStyle=="dots-circular-double") border-radius: 50%; @endif
+    #{{$id}} .{{$dotsStyle}} .owl-dots .owl-dot.active span {
+       border: 2px solid var(--{{$dotsStyleColor}});
+       background-color: transparent;
+     }
+    #{{$id}} .{{$dotsStyle}} .owl-dots .owl-dot.active span:after{
+       width: {{$dotsSize - 8 }}px;
+       height: {{$dotsSize - 8 }}px;
+       background-color: var(--{{$dotsStyleColor}});
+       bottom: 2px !important;
+       left: 2px !important;
+       content: "";
+       position: absolute;
+       @if($dotsStyle=="dots-circular-double") border-radius: 50%; @endif
         }
     @endif
     @endif
@@ -365,33 +341,33 @@
     @if($navOld)
     #{{$id}} .owl-nav [class*=owl-]:hover,
     #{{$id}} .owl-nav [class*=owl-]:focus {
-        background: transparent;
-        outline: 0;
-    }
+       background: transparent;
+       outline: 0;
+     }
     @endif
 
     #{{$id}} .title-section .title {
-       font-size: {{$owlTitleSize}}px;
-       letter-spacing: {{$owlTitleLetterSpacing}}px;
-    }
+           font-size: {{$owlTitleSize}}px;
+           letter-spacing: {{$owlTitleLetterSpacing}}px;
+         }
     #{{$id}} .title-section .subtitle {
-      font-size: {{$owlSubtitleSize}}px;
-      letter-spacing: {{$owlSubtitleLetterSpacing}}px;
-    }
+       font-size: {{$owlSubtitleSize}}px;
+       letter-spacing: {{$owlSubtitleLetterSpacing}}px;
+     }
 
     @if($owlWithLineTitle==1)
     #{{$id}} .title-section .title:after {
-        content: '';
-        display: block;
-        @foreach($owlLineTitleConfig as $key => $line)
-        {{$key}}: {{$line}};
-        @endforeach
-    }
+           content: '';
+           display: block;
+         @foreach($owlLineTitleConfig as $key => $line)
+         {{$key}}: {{$line}};
+         @endforeach
+}
     @endif
     @if($owlWithLineTitle==2)
     #{{$id}} .title-section .subtitle:after {
-         content: '';
-         display: block;
+           content: '';
+           display: block;
          @foreach($owlLineTitleConfig as $key => $line)
         {{$key}}: {{$line}};
          @endforeach
@@ -399,8 +375,8 @@
     @endif
 
     #{{$id}} .position-absolute-left {
-        position: absolute; left: 0;
-    }
+           position: absolute; left: 0;
+         }
 
   </style>
 @endif
