7380b7da2a21f0abefe9a4e74014e3d9790b1388
William <williamverde10@gmail.com>
2025-09-05 10:12:36 -0400

    Update: contact type ICONS

--- app/Models/ContactType.php
+++ app/Models/ContactType.php
@@ -26,62 +26,62 @@ class ContactType extends CoreStaticModel
       self::PHONE => [
         'id' => self::PHONE,
         'title' => itrans('isite::contacttype.phone'),
-        'icon' => 'material-symbols:call'
+        'icon' => 'fa-solid fa-phone'
       ],
       self::ADDRESS => [
         'id' => self::ADDRESS,
         'title' => itrans('isite::contacttype.address'),
-        'icon' => 'material-symbols:location-on'
+        'icon' => 'fa-solid fa-location-dot'
       ],
       self::EMAIL => [
         'id' => self::EMAIL,
         'title' => itrans('isite::contacttype.email'),
-        'icon' => 'material-symbols:mail-outline'
+        'icon' => 'fa-solid fa-envelope'
       ],
       self::FACEBOOK => [
         'id' => self::FACEBOOK,
         'title' => itrans('isite::contacttype.facebook'),
-        'icon' => 'ic:baseline-facebook'
+        'icon' => 'fa-brands fa-facebook'
       ],
       self::TWITTER => [
         'id' => self::TWITTER,
         'title' => itrans('isite::contacttype.twitter'),
-        'icon' => 'line-md:twitter-x'
+        'icon' => 'fa-brands fa-x-twitter'
       ],
       self::INSTAGRAM => [
         'id' => self::INSTAGRAM,
         'title' => itrans('isite::contacttype.instagram'),
-        'icon' => 'mdi:instagram'
+        'icon' => 'fa-brands fa-instagram'
       ],
       self::TIKTOK => [
         'id' => self::TIKTOK,
         'title' => itrans('isite::contacttype.tiktok'),
-        'icon' => 'ic:baseline-tiktok'
+        'icon' => 'fa-brands fa-tiktok'
       ],
       self::YOUTUBE => [
         'id' => self::YOUTUBE,
         'title' => itrans('isite::contacttype.youtube'),
-        'icon' => 'line-md:youtube-filled'
+        'icon' => 'fa-brands fa-youtube'
       ],
       self::LINKEDIN => [
         'id' => self::LINKEDIN,
         'title' => itrans('isite::contacttype.linkedin'),
-        'icon' => 'mdi:linkedin'
+        'icon' => 'fa-brands fa-linkedin'
       ],
       self::GOOGLE => [
         'id' => self::GOOGLE,
         'title' => itrans('isite::contacttype.google'),
-        'icon' => 'mynaui:brand-google-solid'
+        'icon' => 'fa-brands fa-google'
       ],
       self::PINTEREST => [
         'id' => self::PINTEREST,
         'title' => itrans('isite::contacttype.pinterest'),
-        'icon' => 'mdi:pinterest'
+        'icon' => 'fa-brands fa-pinterest'
       ],
       self::FLICKR => [
         'id' => self::FLICKR,
         'title' => itrans('isite::contacttype.flickr'),
-        'icon' => 'mdi:flickr'
+        'icon' => 'fa-brands fa-flickr'
       ],
     ];
   }
